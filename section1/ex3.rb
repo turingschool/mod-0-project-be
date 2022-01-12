ad2ffe1b27257f45496a929e01ad643fd53c11e5
Bryce Wein <bryce.wein@gmail.com>
2022-01-12 15:28:10 -0500

    Refactor Comments

--- section1/ex3.rb
+++ section1/ex3.rb
@@ -1,25 +1,25 @@
-#Prints string to terminal
+# Prints "I will now county my chickens:" to terminal
 puts "I will now county my chickens:"
 # #{} allows for math interpolation
-#Utilized interpolation to count how many of each we have and then print in a string
+# Utilized interpolation to count how many of each we have and then print in a string
 puts "Hens #{25 + 30 / 6}"
 puts "Roosters #{100 - 25 *3 %4}"
-#Prints string to terminal
+# Prints "Now I will count the eggs:" to terminal
 puts "Now I will count the eggs:"
-# this is using math as normal, utilizing normal operations and then returning a value
+# This is using math as normal, utilizing normal operations and then returning a value
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 # this is an example of not using interpolation, and that it will not do the computation
 puts "Is it true that 3 + 2 < 5 - 7?"
-#Using math as normal
+# Using math as normal
 puts 3 + 2 < 5 - 7
-# this is a combo of no math interpolation, followed by interpolation
+# This is a combo of no math interpolation, followed by interpolation
 puts "What is 3 +2? #{3+2}"
 puts "What is 5 - 7? #{5 - 7}"
-#Print string to terminal
+# Print "Oh, that's why it's false." to terminal
 puts "Oh, that's why it's false."
-#Print string to terminal
+# Print "How about some more." to terminal
 puts "How about some more."
-# this is using a boolean operator to return a "true" or "false" value after the math interpolated computation
+# This is using a boolean operator to return a "true" or "false" value after the math interpolated computation
 puts "Is it greater? #{5 > -2}"
 puts "is it greater or equal? #{5 >= -2}"
 puts "Is it less or equal? #{5 <= -2}"
@@ -36,29 +36,29 @@ puts "Is it less or equal? #{5 <= -2}"
 #Study Drill 3
 #Complete
 
-#Study Drill 4
-#Prints string to terminal
+# Study Drill 4
+# Prints "I will now county my chickens:" to terminal
 puts "I will now county my chickens:"
 # #{} allows for math interpolation
-#Utilized interpolation to count how many of each we have and then print in a string
+# Utilized interpolation to count how many of each we have and then print in a string
 puts "Hens #{25.0 + 30.0 / 6.0}"
 puts "Roosters #{100.0 - 25.0 *3.0 %4.0}"
-#Prints string to terminal
+# Prints "Now I will count the eggs:" to terminal
 puts "Now I will count the eggs:"
-# this is using math as normal, utilizing normal operations and then returning a value
+# This is using math as normal, utilizing normal operations and then returning a value
 puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
-# this is an example of not using interpolation, and that it will not do the computation
+# This is an example of not using interpolation, and that it will not do the computation
 puts "Is it true that 3 + 2 < 5 - 7?"
-#Using math as normal
+# Using math as normal
 puts 3.0 + 2.0 < 5.0 - 7.0
-# this is a combo of no math interpolation, followed by interpolation
+# This is a combo of no math interpolation, followed by interpolation
 puts "What is 3 +2? #{3.0+2.0}"
 puts "What is 5 - 7? #{5.0 - 7.0}"
-#Print string to terminal
+# Print "Oh, that's why it's false." to terminal
 puts "Oh, that's why it's false."
-#Print string to terminal
+# Print "How about some more." to terminal
 puts "How about some more."
-# this is using a boolean operator to return a "true" or "false" value after the math interpolated computation
+# This is using a boolean operator to return a "true" or "false" value after the math interpolated computation
 puts "Is it greater? #{5.0 > -2.0}"
 puts "is it greater or equal? #{5.0 >= -2.0}"
 puts "Is it less or equal? #{5.0 <= -2.0}"
