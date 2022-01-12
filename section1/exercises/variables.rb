a35204d0e2781d1af2f1e8cb86f91874dd15ec16
Drew Proebstel <drewproebstel@Drews-MacBook-Pro.local>
2022-01-11 17:08:10 -0800

    Add Section 1

--- section1/exercises/variables.rb
+++ section1/exercises/variables.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section1/exercises/variables.rb`
 
 # Example: Write code that saves your name to a variable and
@@ -11,39 +11,58 @@ p name
 # Write code that saves the string 'Dobby' to a variable and
 # prints what that variable holds to the terminal:
 house_elf = "Dobby"
-# YOUR CODE HERE
+p house_elf
 
 # Write code that saves the string 'Harry Potter must not return to Hogwarts!'
 # and prints what that variable holds to the terminal:
-# YOUR CODE HERE
-
+decree = "Harry Potter must not return to Hogwarts!"
+p decree
 # Write code that adds 2 to the `students` variable and
 # prints the result:
 students = 22
+students = students + 2
 # YOUR CODE HERE
 p students
 
 # Write code that subracts 2 from the `students` variable and
 # prints the result:
-# YOUR CODE HERE
+students = students - 2
 p students
 
 
 # YOU DO:
-# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`. 
+# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`.
 # Store the appropriate data types in each.
-# print all three variables to the terminal.
+# print all three variables to the terminal
 
+first_name = "Drew"
+is_hungry = false
+number_of_pets = 0
+
+p first_name
+p is_hungry
+p number_of_pets
 # IN WORDS:
-# How did you decide to use the data type you did for each of the three variables above? 
+# How did you decide to use the data type you did for each of the three variables above?
 
 # Explain.
 
+#first_name needs to be a string because it is a collection of letters
+#is_hungry is boolean because it can only be true or false
+#number_of_pets is a variable because it is a number
 
 # YOU DO:
 # Re-assign the values to the three variables from the previous challenge to
 # different values (but same data type).
 # Print all three variables to the terminal.
+
+first_name = "Werd"
+is_hungry = true
+number_of_pets = 1000000
+
+p first_name
+p is_hungry
+p number_of_pets
 
 
 # YOU DO:
@@ -51,10 +70,10 @@ p students
 healthy_snacks = 6;
 junk_food_snacks = 8;
 
-
+p healthy_snacks + junk_food_snacks
 #-------------------
 # FINAL CHECK
 #-------------------
 
 # Did you run this file in your terminal to make sure everything printed out
-# to the terminal as you would expect? 
+# to the terminal as you would expect?
