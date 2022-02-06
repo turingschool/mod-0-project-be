ea812f5947de1e61a397ede11cd30c9b23a62779
Stirling Hostetter <stirhoss@icloud.com>
2022-02-06 16:06:33 -0600

    Complete variable.rb

--- section1/exercises/variables.rb
+++ section1/exercises/variables.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section1/exercises/variables.rb`
 
 # Example: Write code that saves your name to a variable and
@@ -11,50 +11,70 @@ p name
 # Write code that saves the string 'Dobby' to a variable and
 # prints what that variable holds to the terminal:
 house_elf = "Dobby"
-# YOUR CODE HERE
+p house_elf
 
 # Write code that saves the string 'Harry Potter must not return to Hogwarts!'
 # and prints what that variable holds to the terminal:
-# YOUR CODE HERE
+warning = "Harry Potter must not return to Hogwarts!"
+p warning
 
 # Write code that adds 2 to the `students` variable and
 # prints the result:
 students = 22
-# YOUR CODE HERE
+students += 2
 p students
 
 # Write code that subracts 2 from the `students` variable and
 # prints the result:
-# YOUR CODE HERE
+students -= 2
 p students
 
 
 # YOU DO:
-# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`. 
+# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`.
 # Store the appropriate data types in each.
 # print all three variables to the terminal.
+first_name = "Stirling"
+is_hungry = true
+number_of_pets = 1
 
+p first_name
+p is_hungry
+p number_of_pets
 # IN WORDS:
-# How did you decide to use the data type you did for each of the three variables above? 
+# How did you decide to use the data type you did for each of the three variables above?
 
 # Explain.
+=begin
+I decided to make first_name a string because it is just a word and has no value as a number or boolean.
+I made is_hungry a boolean so I could use it as a simple true or false evaluation.
+I made number of pets an integer becuase it is just a whole number.
 
+=end
 
 # YOU DO:
 # Re-assign the values to the three variables from the previous challenge to
 # different values (but same data type).
 # Print all three variables to the terminal.
+first_name = "Mark"
+is_hungry = false
+number_of_pets = 5
 
-
+p first_name
+p is_hungry
+p number_of_pets
 # YOU DO:
 # Using the variables below, print the total number of snacks to the terminal:
 healthy_snacks = 6;
 junk_food_snacks = 8;
 
-
+total_snacks = healthy_snacks + junk_food_snacks
+p total_snacks
 #-------------------
 # FINAL CHECK
 #-------------------
 
 # Did you run this file in your terminal to make sure everything printed out
-# to the terminal as you would expect? 
+# to the terminal as you would expect?
+
+# Yes
