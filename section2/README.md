333642c714023c3225dfd44f2c2dd904a265039f
Stirling Hostetter <stirhoss@icloud.com>
2022-02-07 15:41:17 -0600

    Complete all but reflection.md

--- section2/README.md
+++ section2/README.md
@@ -49,7 +49,7 @@ When you are all done with the lessons, exercises, and questions for today, you 
 
     - [ ] [Making Decisions](https://learnrubythehardway.org/book/ex31.html) from Learn Ruby the Hard Way.
 
-    - [ ] [Conditionals](http://tutorials.jumpstartlab.com/projects/ruby_in_100_minutes.html#9.-conditionals) from Ruby in 100 Minutes.
+    - [x] [Conditionals](http://tutorials.jumpstartlab.com/projects/ruby_in_100_minutes.html#9.-conditionals) from Ruby in 100 Minutes.
 
 1. Work through the `if_statements.rb` file in the `section2/exercises` directory.
 
--- section2/exercises/methods.rb
+++ section2/exercises/methods.rb
@@ -12,21 +12,35 @@ print_name
 
 # Write a method that takes a name as an argument and prints it:
 def print_name(name)
-  # YOUR CODE HERE
+  p name
 end
 
 print_name("Albus Dumbledore")
 
-# Write a method that takes in 2 numbers as arguments and prints 
+# Write a method that takes in 2 numbers as arguments and prints
 # their sum. Then call your method three times with different arguments passed in:
 # YOUR CODE HERE
 
+def sum(x , y)
+  p (x + y)
+end
 
-# Write a method that takes in two strings as arguments and prints 
-# a concatenation of those two strings. Example: The arguments could be 
-# (man, woman) and the end result might output: "When Harry Met Sally".  
-# Then call your method three times with different arguments passed in. 
+sum(5 , 6)
+sum(10 , 5)
+sum(22 , -10)
 
+# Write a method that takes in two strings as arguments and prints
+# a concatenation of those two strings. Example: The arguments could be
+# (man, woman) and the end result might output: "When Harry Met Sally".
+# Then call your method three times with different arguments passed in.
+
+def raining(animal1 , animal2)
+  p "It's raining #{animal1} and #{animal2}!"
+end
+
+raining("cats", "dogs")
+raining("mice", "sheep")
+raining("turtles", "goats")
 
 #-------------------
 # PART 3: Naming is Hard
@@ -39,7 +53,11 @@ print_name("Albus Dumbledore")
 
 # Look at the code you wrote for the previous YOU DO🎈 - what did you name the
 # function, and why?
-# YOUR ANSWER HERE
+# I named the function raining to refer to the saying that I was going to be using.
+# This is the first word you think of when you think of the saying so I thouhgt
+# it made sense as a function name. 
 
 # What did you name each parameter, and why?
-# YOUR ANSWER HERE
+# I named the parameters animal1 and animal2 becuase there isn't really anything
+# unique or important about the order they fall in. It is just important that you
+# name 2 of them.
