0053e9fec38f59bf6384e7e293456bd1b732cbbf
Stephen Wilkens <stephenwilkens@gmail.com>
2022-02-08 10:01:12 -0700

    add completed ex1

---
+++ section1/ex1.rb
@@ -0,0 +1,16 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'"
+puts 'I "said" do not touch this.'
+# Make your script print another line.
+puts "Here's another line for ya"
+# Make your script print only one of the lines.
+# Is this supposed to be a specific Ruby command or is the intention just to use
+#octothorpes to make the other lines not print?
+# Put a # (octothorpe) character at the beginning of a line. What did it do?
+#Try to figure out what this does.
+# Octothorpes will keep whatever follows it from printing. Good way to leave
+# notes or hide a piece of code. 
