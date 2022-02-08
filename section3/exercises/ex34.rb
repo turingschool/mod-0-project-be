d9d15ab8f5a873efd548bf9d23bd80f13b28532d
Stirling Hostetter <stirhoss@icloud.com>
2022-02-08 12:26:22 -0600

    Complete ex34.rb

--- section3/exercises/ex34.rb
+++ section3/exercises/ex34.rb
@@ -1,1 +1,21 @@
 animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+=begin
+The animal at 1. = "ruby"
+The third (3rd) animal. = "peacock"
+The first (1st) animal. = "bear"
+The animal at 3. = "Kangaroo"
+The fifth (5th) animal. = "Whale"
+The animal at 2. = "peacock"
+The sixth (6th) animal. = "platypus"
+The animal at 4. = "Whale"
+=end
+
+# The animal at 1 is ruby and is the second animal.
+# The third animal is at 2 and is a peacock.
+# The fist animal is at 0 and is a bear.
+# The animal at 3 is kangaroo and is the fourth animal.
+# The fith animal is at 4 and is a Whale.
+# The animal at 2 is peacock and is the third animal.
+# The sixth animal is at 5 and is a platypus.
+# The animal at 4 is whale and is the fith animal.
