9eb937f6a59cbe1a3c414dd61c4e23d9fcaada2b
Stephen Wilkens <stephenwilkens@gmail.com>
2022-02-08 10:02:30 -0700

    add completed ex3 and ex3 question 3 file

---
+++ section1/ex3.rb
@@ -0,0 +1,48 @@
+# Printing a string
+puts "I will now count my chickens:"
+
+# printing a string with an equation in it
+puts "Hens #{25.0 + 30.0 / 6.0}"
+# printing a string with an equation in it. I need to look into the modulus more
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+# printing a string
+puts "Now I will count the eggs"
+# printing the answer to an equation
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# How does it know to print that this is false from just this string?
+# Below we had to put the equation in brackets to get it to print the
+# answer
+# "Is it" command?
+puts "Is it true that 3.0 + 2.0 < 5.0 -7.0?"
+#Printing an equation
+puts 3.0 + 2.0 < 5.0 - 7.0
+# printing a string with a question and using an equation imbedded in the string
+# to anser the question
+puts "What is 3.0 + 2.0? #{3.0 + 2.0}"
+# printing a string with a question and using an equation imbedded in the string
+# to anser the question
+puts "What is 5.0 - 7.0? #{5.0 - 7.0}"
+# printing a message
+puts "Oh, that's why it's false"
+# printing another message
+puts "How about some more."
+# using the "is it" command to ask true or false question about the equation in
+# the string
+puts "Is it greater? #{5.0 > -2.0}"
+# using the "is it" command to ask true or false question about the equation in
+# the string
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+# using the "is it" command to ask true or false question about the equation in
+# the string
+puts "Is it less or equal? #{5.0 <= -2.0}"
+
+
+# 1) Above each line, use the # to write a comment to yourself explaining what
+# the line does. 👍🏼
+# 2) Remember in Exercise 0 when you started irb? Start irb this way again and,
+# using the math operators, use Ruby as a calculator. 👍🏼
+# 3) Find something you need to calculate and write a new .rb file that does it.
+# ^ ex3q3.rm
+# 4) Rewrite ex3.rb to use floating point numbers so it's more accurate. 20.0 is
+# floating point. 👍🏼
---
+++ section1/ex3q3.rb
@@ -0,0 +1,6 @@
+length = 30
+width = 18
+height = 1
+volume = length * width * height
+
+puts "the volume of my desk top is #{volume} inches"
