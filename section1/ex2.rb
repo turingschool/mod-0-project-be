51e7b5026b9cb02deff432eb236dfc00cbdc3153
Stephen Wilkens <stephenwilkens@gmail.com>
2022-02-08 10:01:38 -0700

    add completed ex2

---
+++ section1/ex2.rb
@@ -0,0 +1,21 @@
+# A comment, this is so you can read your code later
+# Anything after the # is ignored by Ruby
+
+puts "I could have code like this"
+# You can also use a comment to "disable" or comment out a piece of code
+# puts 'This wont run'
+
+puts "This will run."
+
+# 1) Find out if you were right about what the # character does and make sure you
+# know what it's called (octothorpe or pound character).
+# I was right and know what it's called
+
+# 2) Take your ex2.rb file and review each line going backward. Start at the last
+# line, and check each word in reverse against what you should have typed.
+# Did you find more mistakes? Fix them.
+# I did not have mistakes for this
+
+# 3) Read what you typed above out loud, including saying each character by its
+# name. Did you find more mistakes? Fix them.
+# I did not find any mistakes
