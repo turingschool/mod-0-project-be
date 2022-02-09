dc2323d6ab177affb6c3fc59fd1fe7305c2e1a12
Lucas Colwell <colwellc37@gmail.com>
2022-02-08 17:42:56 -0700

    finish exercise 2

---
+++ section1/ex2.rb
@@ -0,0 +1,9 @@
+# comment
+# # causes ruby to ignore text.
+
+puts "I could have a code like this" # then the comment after is ignored
+
+#you can also use a comment to "disable" a or comment out a piece of code
+# puts "this won't run."
+
+puts "This will run"
