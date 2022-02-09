9193beb602030ebe2d1614ae955179a5f1e639b0
Lucas Colwell <colwellc37@gmail.com>
2022-02-08 18:49:25 -0700

    Do study drills

--- section1/ex4.rb
+++ section1/ex4.rb
@@ -1,6 +1,9 @@
 cars = 100
+  # Technically you can use a float because there are 4 spaces in a car
+  # This could apply if later variable such as bags take up half a space
+  # If it said people_allowed_in_car you probably would not use a float
 space_in_a_car = 4.0
-drivers = ro
+drivers = 30
 passengers = 90
 cars_not_driven = cars - drivers
 cars_driven + drivers
@@ -14,3 +17,5 @@ puts "There will be #{cars_not_driven} empty cars today."
 puts "We can transport #{carpool_capacity} people today."
 puts "We have #{passengers} to carpool today."
 puts "We need to put about #{average_passengers_per_car} in each car."
+
+#study drill answer 1: they forgot to define the variable carpool_capacity in line 7.
