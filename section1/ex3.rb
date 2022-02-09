871c0df6a33710d41428bc8d4ee565aaac09421f
Lucas Colwell <colwellc37@gmail.com>
2022-02-08 18:27:42 -0700

    Change integers to floats

--- section1/ex3.rb
+++ section1/ex3.rb
@@ -1,24 +1,24 @@
 puts "I will now count my chickens:"
   # this does math inside the string
-puts "Hens #{25 + 30 / 6}"
-puts "Roosters #{100 - 25 * 3 % 4}"
+puts "Hens #{25.00 + 30.00 / 6.00}"
+puts "Roosters #{100.00 - 25.00 * 3.00 % 4.00}"
   # string
 puts "Now I will count the eggs:"
-  # math using integers
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+  # math using floats
+puts 3.00 + 2.00 + 1.00 - 5.00 + 4.00 % 2.00 - 1.00 / 4.00 + 6.00
   # string
-puts "Is it true that 3 + 2 < 5 - 7?"
+puts "Is it true that 3.00 + 2.00 < 5.00 - 7.00?"
   # boolean using <
-puts 3 + 2 < 5 - 7
+puts 3.00 + 2.00 < 5.00 - 7.00
   # strings with math performed
-puts "What is 3 + 2? #{3 + 2}"
-puts "What is 5 - 7? #{5 - 7}"
+puts "What is 3.00 + 2.00? #{3.00 + 2.00}"
+puts "What is 5.00 - 7.00? #{5.00 - 7.00}"
 
 puts "Oh, that's why it's false."
 
 puts "How about some more."
 
   # strings with boolean
-puts "Is it greater? #{5 > -2}"
-puts "Is it greater or equal? #{5 >= -2}"
-puts "Is it less or equal? #{5 <= -2}"
+puts "Is it greater? #{5.00 > -2.00}"
+puts "Is it greater or equal? #{5.00 >= -2.00}"
+puts "Is it less or equal? #{5.00 <= -2.00}"
