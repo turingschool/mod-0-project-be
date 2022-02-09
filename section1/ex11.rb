8468691cf87745994f93421e4f1ad1a0de2df624
Lucas Colwell <colwellc37@gmail.com>
2022-02-08 19:35:12 -0700

    Do study drill

--- section1/ex11.rb
+++ section1/ex11.rb
@@ -7,3 +7,9 @@ weight = gets.chomp
 
 # added [years]
 puts "So, you're #{age} [years] old, #{height} tall and #{weight} heavy"
+
+  #Study drills
+print "What is your dog's name? "
+dog_name = gets.chomp
+
+puts "#{dog_name} is a great name for a dog!"
