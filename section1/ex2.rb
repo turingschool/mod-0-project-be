72171fc375b76a8bc329e4271105c1f2559e9522
Lucas Colwell <colwellc37@gmail.com>
2022-02-08 17:48:20 -0700

    Do study drill

--- section1/ex2.rb
+++ section1/ex2.rb
@@ -1,9 +1,13 @@
 # comment
 # # causes ruby to ignore text.
 
-puts "I could have a code like this" # then the comment after is ignored
+puts "I could have a code like this." # then the comment after is ignored
 
 #you can also use a comment to "disable" a or comment out a piece of code
 # puts "this won't run."
 
-puts "This will run"
+puts "This will run."
+
+# comments were typed differently than in example
+# code was followed and edited to be exactly the same
+# sorry to personalize
