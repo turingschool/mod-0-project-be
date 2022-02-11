3d13bec058ea9fd3fcf45c0a44705bbd7ccc2a17
Stephen Wilkens <stephenwilkens@gmail.com>
2022-02-11 15:32:22 -0700

    add ex34

---
+++ section3/exercises/ex34.rb
@@ -0,0 +1,8 @@
+#1) With what you know of the difference between these types of numbers, can you
+# explain why the year 2010 in "January 1, 2010," really is 2010 and not 2009?
+# (Hint: you can't pick years at random.)
+# =>  Because years are ordered as an index.
+
+# 2)Write some more arrays and work out similar indexes until you can translate them.
+candy = ["snickers", "m&ms", "1000grand", "Sour Patch Kids"]
+#m&ms = candy[1]
