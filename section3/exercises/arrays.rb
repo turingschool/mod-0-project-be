dfaa9b6fc89ce690c1e4ca16127c46f650d97ce3
philmarcu <philmark18@gmail.com>
2022-02-11 16:51:59 -0700

    Add in Array & Loop exercises

--- section3/exercises/arrays.rb
+++ section3/exercises/arrays.rb
@@ -5,7 +5,7 @@
 # Two examples are already completed. Your task is to complete
 # any remaining prompt.
 
-# To check your work, run this file by entering the following 
+# To check your work, run this file by entering the following
 # command in your terminal:
 # `ruby section3/exercises/arrays.rb``
 
@@ -17,14 +17,15 @@
 # EXAMPLE: write code below that will print an array of animals.
 # Store the array in a variable.
 animals = ["Zebra", "Giraffe", "Elephant"]
-print animals
+puts animals
 
 # EXAMPLE: Write code below that will print "Zebra" from the animals array
 # YOUR CODE HERE
-print animals[0]
-
+puts animals[0]
 # YOU DO: Write code below that will print the number of elements in array of
 # animals from above.
+
+
 
 
 # YOU DO: Write code that will reassign the last item in the animals
@@ -56,7 +57,7 @@ print animals[0]
 # print the changed array to verify that item has been removed
 
 
-# YOU DO: Write code to add 3 new foods to the array. 
+# YOU DO: Write code to add 3 new foods to the array.
   # There are several ways to do this - choose whichever you'd like!
 # Then, print the changed array to verify the new items have been added
 
@@ -79,11 +80,9 @@ print animals[0]
 posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"]
 
 # YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-# may be storing data? Come up with 3 examples - they could be from different web applications or 
+# may be storing data? Come up with 3 examples - they could be from different web applications or
 # all from the same one.
 
 # 1:
 # 2:
 # 3:
-
-
