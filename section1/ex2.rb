85adf05efe4abf9a9a72d09742cb85bc8498d57f
Stephen Wilkens <stephenwilkens@gmail.com>
2022-02-12 12:26:35 -0700

    add refactored ex2

--- section1/ex2.rb
+++ section1/ex2.rb
@@ -9,13 +9,13 @@ puts "This will run."
 
 # 1) Find out if you were right about what the # character does and make sure you
 # know what it's called (octothorpe or pound character).
-# I was right and know what it's called
+# I was right and know what it's called.
 
 # 2) Take your ex2.rb file and review each line going backward. Start at the last
 # line, and check each word in reverse against what you should have typed.
 # Did you find more mistakes? Fix them.
-# I did not have mistakes for this
+# I did not have mistakes for this.
 
 # 3) Read what you typed above out loud, including saying each character by its
 # name. Did you find more mistakes? Fix them.
-# I did not find any mistakes
+# I did not find any mistakes.
