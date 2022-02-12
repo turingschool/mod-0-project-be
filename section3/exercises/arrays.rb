50dbff1383e2df5d1f10777674082249dab55d18
Lucas Colwell <colwellc37@gmail.com>
2022-02-12 13:40:45 -0700

    Complete arrays

--- section3/exercises/arrays.rb
+++ section3/exercises/arrays.rb
@@ -5,7 +5,7 @@
 # Two examples are already completed. Your task is to complete
 # any remaining prompt.
 
-# To check your work, run this file by entering the following 
+# To check your work, run this file by entering the following
 # command in your terminal:
 # `ruby section3/exercises/arrays.rb``
 
@@ -21,46 +21,71 @@ print animals
 
 # EXAMPLE: Write code below that will print "Zebra" from the animals array
 # YOUR CODE HERE
-print animals[0]
+puts ""
+puts animals[0]
 
 # YOU DO: Write code below that will print the number of elements in array of
 # animals from above.
-
+puts ""
+puts animals.count
 
 # YOU DO: Write code that will reassign the last item in the animals
 # array to "Gorilla"
-
-
+puts ""
+animals[2] = "Gorilla"
+puts "position 2 has been changed to #{animals[2]}"
+puts ""
 # YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+animals.push("Fish")
+print animals
+puts ""
+puts ""
 
 # YOU DO: Write code that will print the String "Elephant" in the animals array
-
-
+puts animals[2]
+puts "the string 'elephant' was replaced by 'Gorilla' as instructed in line 32-33'..."
+puts ""
 #-------------------
 # PART 2: Foods: Array Methods
 #-------------------
 
 # YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
 
+foods = ["pizza", "pasta", "burgers", "pudding"]
 
 # YOU DO: Write code below that will print the number of elements in the array of
 # foods from above.
 
+puts "There are #{foods.count} elements in the array 'foods'"
 
 # YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 # print the changed array to verify "broccoli" has been added
 
+foods.push("broccoli")
+puts "new array: #{foods}"
 
 # YOU DO: Write code below that removes the last item of food from the foods array and
 # print the changed array to verify that item has been removed
+puts ""
+foods.pop
+puts "new array: #{foods}"
+puts ""
 
-
-# YOU DO: Write code to add 3 new foods to the array. 
+# YOU DO: Write code to add 3 new foods to the array.
   # There are several ways to do this - choose whichever you'd like!
 # Then, print the changed array to verify the new items have been added
-
+foods.unshift("ice cream")
+foods.push("oranges")
+foods.push("hot dogs")
+print foods
+puts ""
+puts ""
 # YOU DO: Remove the food that is in index position 0.
+
+foods.shift
+print foods
+puts ""
+puts ""
 
 #-------------------
 # PART 3: Where are Arrays used?
@@ -79,11 +104,12 @@ print animals[0]
 posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"]
 
 # YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-# may be storing data? Come up with 3 examples - they could be from different web applications or 
+# may be storing data? Come up with 3 examples - they could be from different web applications or
 # all from the same one.
 
 # 1:
+genres = ["Electronic", "Jazz", "Classical"]
 # 2:
+message_folders = ["college friends", "family", "coworkers", "school"]
 # 3:
-
-
+piano_music_folders = ["pieces to learn", "pieces learned"]
