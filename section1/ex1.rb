8178cf8430da48b97d417f2f736976adbd41193c
Stephen Wilkens <stephenwilkens@gmail.com>
2022-02-12 12:24:06 -0700

    Add refactored ex1.rb

--- section1/ex1.rb
+++ section1/ex1.rb
@@ -1,16 +1,17 @@
-puts "Hello World!"
-puts "Hello Again"
-puts "I like typing this."
-puts "This is fun."
-puts "Yay! Printing."
-puts "I'd much rather you 'not'"
-puts 'I "said" do not touch this.'
-# Make your script print another line.
+#puts "Hello World!"
+#puts "Hello Again"
+#puts "I like typing this."
+#puts "This is fun."
+#puts "Yay! Printing."
+#puts "I'd much rather you 'not'"
+#puts 'I "said" do not touch this.'
+
+#1) Make your script print another line.
 puts "Here's another line for ya"
-# Make your script print only one of the lines.
-# Is this supposed to be a specific Ruby command or is the intention just to use
-#octothorpes to make the other lines not print?
-# Put a # (octothorpe) character at the beginning of a line. What did it do?
-#Try to figure out what this does.
-# Octothorpes will keep whatever follows it from printing. Good way to leave
-# notes or hide a piece of code. 
+
+#2) Make your script print only one of the lines.
+# I did this by using an octothorpe to block out the previous lines
+
+#3)Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
+# This character will make the text following it not show up as code. It can be
+# used to make notes or tell ruby not to pay attention to a line of code. 
