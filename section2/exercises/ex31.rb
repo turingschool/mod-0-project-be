ad7100c959cd29811cff8f29f37fe1222fce22da
Lucas Colwell <colwellc37@gmail.com>
2022-02-12 09:47:19 -0700

    Do study drill 1

--- section2/exercises/ex31.rb
+++ section2/exercises/ex31.rb
@@ -1,4 +1,4 @@
-puts "You enter a dark room with two doors.  Do you go through door #1 or door #2?"
+puts "You enter a dark room with three doors.  Do you go through door #1 or door #2?"
 
 print "> "
 door = $stdin.gets.chomp
@@ -7,6 +7,7 @@ if door == "1"
   puts "There's a giant bear here eating a cheese cake.  What do you do?"
   puts "1.  Take the cake."
   puts "2.  Scream at the bear."
+  puts "3.  Smash cake in bear's face and laugh"
 
   print "> "
   bear = $stdin.gets.chomp
@@ -15,6 +16,9 @@ if door == "1"
     puts "The bear eats your face off.  Good job!"
   elsif bear == "2"
     puts "The bear eats your legs off.  Good job!"
+# felt a need to add a positive option
+  elsif bear == "3"
+    puts "The bear has a sense of humor and laughs. You both eat cake."
   else
     # in this line, ""%s" calls the value of "bear" (user input)
     puts "Well, doing %s is probably better.  Bear runs away." % bear
@@ -34,6 +38,9 @@ elsif door == "2"
   else
     puts "The insanity rots your eyes into a pool of muck.  Good job!"
   end
+
+elsif door == "3"
+  puts "A clown appears and throws a pie in your face."
 
 else
   puts "You stumble around and fall on a knife and die.  Good job!"
