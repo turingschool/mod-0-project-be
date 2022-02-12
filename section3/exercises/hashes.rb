7a9b55435d7dcef88445e2e83e341716064fc707
Caden Jarrett <caden.jarrett99@gmail.com>
2022-02-12 09:50:15 -0700

    Added section r excercises and lesson work

--- section3/exercises/hashes.rb
+++ section3/exercises/hashes.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section3/exercises/hashes.rb`
 
 # Example: Write code that prints a hash holding grocery store inventory:
@@ -8,24 +8,28 @@ foods = {apples: 23, grapes: 507, eggs: 48}
 p foods
 
 # Write code that prints a hash holding zoo animal inventory:
-zoo = #YOUR CODE HERE
+zoo = {lions: 5, tigers: 7, bears: 3, monkeys: 15}
 p zoo
 
-# Write code that prints all of the 'keys' of the zoo variable 
+# Write code that prints all of the 'keys' of the zoo variable
 # you created above:
 # YOUR CODE HERE
+p zoo.keys
 
-# Write code that prints all of the 'values' of the zoo variable 
+# Write code that prints all of the 'values' of the zoo variable
+# you created above:
+p zoo.values
+
+# Write code that prints the value of the first animal of the zoo variable
 # you created above:
 # YOUR CODE HERE
+p zoo.fetch(:lions)
 
-# Write code that prints the value of the first animal of the zoo variable 
-# you created above:
-# YOUR CODE HERE
-
-# Write code that adds an animal to the zoo hash. 
+# Write code that adds an animal to the zoo hash.
 # Then, print the updated hash:
 # YOUR CODE HERE
+p zoo.store(:alligators, 4)
+p zoo
 
 
 #-------------------
@@ -38,17 +42,20 @@ p zoo
 
 # Declare a variable that stores hash. Each key should be an attribute of an email and each
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
+emails = {southwest: "upcoming flight", usaa: "withdrawal alert", disney: 'new episodes added', venmo: 'new transaction', gemini: 'new deposits available'}
 
 # Write code that prints your email hash to the terminal.
+p emails
 
-
-# Write code that prints all of the 'keys' of the email hash 
+# Write code that prints all of the 'keys' of the email hash
 # you created above:
 # YOUR CODE HERE
+p emails.keys
 
-# Write code that prints all of the 'values' of the email hash 
+# Write code that prints all of the 'values' of the email hash
 # you created above:
 # YOUR CODE HERE
+p emails.values
 
 
 #-------------------
@@ -76,7 +83,7 @@ posts = [
     timestamp: "4:37 PM August 13, 2019",
     number_likes: 0,
     comments: []
-  }, 
+  },
   {
     image_src: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -90,12 +97,15 @@ p posts
 p posts[0]
 
 
-# The code snippet above shows an Array with 2 elements. Each element in the Array is a 
-# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK 
+# The code snippet above shows an Array with 2 elements. Each element in the Array is a
+# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK
 # a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 # good to have some exposure before going into Mod 1.
 
 
-# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same 
+# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same
 # key-value pairs you used in your email Hash above.
 # Then, print the email Array to the terminal.
+
+states = {"CO" => "Colorado", "IA" => "Iowa", "OK" => "Oklahoma"}
+p states.fetch("IA")
