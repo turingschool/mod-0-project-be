550c9aeb29a6f08e386ce267298b1ce7e2296751
Lucas Colwell <colwellc37@gmail.com>
2022-02-12 15:02:48 -0700

    Complete hashes part 1

--- section3/exercises/hashes.rb
+++ section3/exercises/hashes.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section3/exercises/hashes.rb`
 
 # Example: Write code that prints a hash holding grocery store inventory:
@@ -8,26 +8,32 @@ foods = {apples: 23, grapes: 507, eggs: 48}
 p foods
 
 # Write code that prints a hash holding zoo animal inventory:
-zoo = #YOUR CODE HERE
+zoo = {tigers: 11, penguins: 37, sloths: 42}
 p zoo
-
-# Write code that prints all of the 'keys' of the zoo variable 
+puts '-' * 10
+# Write code that prints all of the 'keys' of the zoo variable
 # you created above:
-# YOUR CODE HERE
-
-# Write code that prints all of the 'values' of the zoo variable 
+zoo.each do |animal, number|
+  puts animal #puts was less messy output in terminal
+end
+puts '-' * 10
+# Write code that prints all of the 'values' of the zoo variable
 # you created above:
-# YOUR CODE HERE
-
-# Write code that prints the value of the first animal of the zoo variable 
+zoo.each do |animal, number|
+  puts number #again, puts was less messy output in terminal
+end
+puts '-' * 10
+# Write code that prints the value of the first animal of the zoo variable
 # you created above:
-# YOUR CODE HERE
-
-# Write code that adds an animal to the zoo hash. 
+puts "The value of the first animal is #{zoo.values[0]}"
+puts '-' * 10
+# Write code that adds an animal to the zoo hash.
 # Then, print the updated hash:
-# YOUR CODE HERE
+zoo["walrus" => 7]
 
+puts zoo
 
+puts '-' * 10
 #-------------------
 # Part 2: Email
 #-------------------
@@ -42,11 +48,11 @@ p zoo
 # Write code that prints your email hash to the terminal.
 
 
-# Write code that prints all of the 'keys' of the email hash 
+# Write code that prints all of the 'keys' of the email hash
 # you created above:
 # YOUR CODE HERE
 
-# Write code that prints all of the 'values' of the email hash 
+# Write code that prints all of the 'values' of the email hash
 # you created above:
 # YOUR CODE HERE
 
@@ -76,7 +82,7 @@ posts = [
     timestamp: "4:37 PM August 13, 2019",
     number_likes: 0,
     comments: []
-  }, 
+  },
   {
     image_src: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -90,12 +96,12 @@ p posts
 p posts[0]
 
 
-# The code snippet above shows an Array with 2 elements. Each element in the Array is a 
-# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK 
+# The code snippet above shows an Array with 2 elements. Each element in the Array is a
+# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK
 # a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 # good to have some exposure before going into Mod 1.
 
 
-# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same 
+# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same
 # key-value pairs you used in your email Hash above.
 # Then, print the email Array to the terminal.
