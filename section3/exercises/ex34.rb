66d5ee343aaf396bd3525347e2a4dbd8ea298ae7
Lucas Colwell <colwellc37@gmail.com>
2022-02-12 12:38:59 -0700

    Do exercise and study drills

---
+++ section3/exercises/ex34.rb
@@ -0,0 +1,41 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts "the animal at 1 is the second animal, the ruby"
+puts "The second animal is at 1 and is the ruby"
+puts "correct answer: #{animals[1]}"
+
+puts "The third animal is at 2 and is the peacock"
+puts "the animal at 2 is the third animal, the peacock"
+puts "correct answer: #{animals[2]}"
+
+puts "The first animal is at 0 and is the bear"
+puts "the animal at 0 is the first animal, the bear"
+puts "correct answer: #{animals[0]}"
+
+puts "the animal at 3 is the fourth animal, the kangaroo"
+puts "The fourth animal is at 3 and is the kangaroo"
+puts "correct answer: #{animals[3]}"
+
+puts "The fifth animal is at 4 and is the whale"
+puts "the animal at 4 is the fifth animal, the whale"
+puts "correct answer: #{animals[4]}"
+
+puts "the animal at 2 is the third animal, the peacock"
+puts "The third animal is at 2 and is the peacock"
+puts "correct answer: #{animals[2]}"
+
+puts "The sixth animal is at 5 and is the platypus"
+puts "the animal at 5 is the sixth animal, the platypus"
+puts "correct answer: #{animals[5]}"
+
+puts "the animal at 4 is the fifth animal, the whale"
+puts "The fifth animal is at 4 and is the whale"
+puts "correct answer: #{animals[4]}"
+
+#January 1 is in position 0 if the month of January was an array.
+
+materials = ["glass", "metal", "water", "sponge"]
+puts "in the array 'materials', position 0 is the first position. the next line should read 'glass'"
+puts "#{materials[0]} is the correct answer"
+puts "In the array 'materials', position -1 is the last position. The next line should read 'sponge'"
+puts "#{materials[-1]} is the correct answer"
