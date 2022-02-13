584ba3be65e8c280d8bbcff9e34ff507116dacea
Deannah Burke <deannah@bierstadtlager.com>
2022-02-12 18:04:41 -0700

    Add more Section 3

--- section3/exercises/arrays.rb
+++ section3/exercises/arrays.rb
@@ -5,7 +5,7 @@
 # Two examples are already completed. Your task is to complete
 # any remaining prompt.
 
-# To check your work, run this file by entering the following 
+# To check your work, run this file by entering the following
 # command in your terminal:
 # `ruby section3/exercises/arrays.rb``
 
@@ -25,42 +25,50 @@ print animals[0]
 
 # YOU DO: Write code below that will print the number of elements in array of
 # animals from above.
+animals.count array.length
+#Both of these methods return the number of elements in the array.
 
 
 # YOU DO: Write code that will reassign the last item in the animals
 # array to "Gorilla"
-
+animals.pop
+animals.push("Gorilla")
 
 # YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+animals.push("Hyena")
 
 # YOU DO: Write code that will print the String "Elephant" in the animals array
-
+#I removed "Elephant" when I reassigned the last item in the animals array to "Gorilla".  I can add "Elephant" back in to its original position 2 and then print with the code that follows.
+animals.insert(2, "Elephant")
+print animals [2]
 
 #-------------------
 # PART 2: Foods: Array Methods
 #-------------------
 
 # YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+best_veggies = ["spinach", "mushrooms", "bell peppers", "squash", "sweet potatoes", "asparagus"]
 
 # YOU DO: Write code below that will print the number of elements in the array of
 # foods from above.
-
+best_veggies.length
 
 # YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 # print the changed array to verify "broccoli" has been added
-
+best_veggies.unshift("broccoli")
 
 # YOU DO: Write code below that removes the last item of food from the foods array and
 # print the changed array to verify that item has been removed
+best_veggies.pop
 
-
-# YOU DO: Write code to add 3 new foods to the array. 
+# YOU DO: Write code to add 3 new foods to the array.
   # There are several ways to do this - choose whichever you'd like!
 # Then, print the changed array to verify the new items have been added
+best_veggies.insert(2, "brussel sprouts", "carrots", "beets")
+print best_veggies
 
 # YOU DO: Remove the food that is in index position 0.
+best_veggies.shift
 
 #-------------------
 # PART 3: Where are Arrays used?
@@ -79,11 +87,11 @@ print animals[0]
 posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"]
 
 # YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-# may be storing data? Come up with 3 examples - they could be from different web applications or 
+# may be storing data? Come up with 3 examples - they could be from different web applications or
 # all from the same one.
 
-# 1:
-# 2:
-# 3:
+# 1: At my current job, we use an app called 7shifts.  Two lists utilized in this app are the list of my shifts each week, and the list of current employees and their contact information.
 
+# 2: I use Spotify quite a bit, lots of lists in there including playlists, artist albums lists, and lists of the songs the user listens to the most.
 
+# 3: I am very new at cooking and have been using Hello Fresh to teach myself.  Each week they have lists of the meal kits available and within each meal there is a list of ingredients and a list of cooking instructions.
--- section3/exercises/hashes.rb
+++ section3/exercises/hashes.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section3/exercises/hashes.rb`
 
 # Example: Write code that prints a hash holding grocery store inventory:
@@ -8,25 +8,26 @@ foods = {apples: 23, grapes: 507, eggs: 48}
 p foods
 
 # Write code that prints a hash holding zoo animal inventory:
-zoo = #YOUR CODE HERE
+zoo = {cheetahs: 2, monkeys: 25, zebras: 4, hippos: 1, meerkats: 18}
 p zoo
 
-# Write code that prints all of the 'keys' of the zoo variable 
-# you created above:
-# YOUR CODE HERE
 
-# Write code that prints all of the 'values' of the zoo variable 
+# Write code that prints all of the 'keys' of the zoo variable
 # you created above:
-# YOUR CODE HERE
+zoo.keys
 
-# Write code that prints the value of the first animal of the zoo variable 
+# Write code that prints all of the 'values' of the zoo variable
 # you created above:
-# YOUR CODE HERE
+zoo.values
 
-# Write code that adds an animal to the zoo hash. 
+# Write code that prints the value of the first animal of the zoo variable
+# you created above:
+zoo.values[0]
+
+# Write code that adds an animal to the zoo hash.
 # Then, print the updated hash:
-# YOUR CODE HERE
-
+zoo[:flamingoes] = 54
+p zoo
 
 #-------------------
 # Part 2: Email
@@ -42,11 +43,11 @@ p zoo
 # Write code that prints your email hash to the terminal.
 
 
-# Write code that prints all of the 'keys' of the email hash 
+# Write code that prints all of the 'keys' of the email hash
 # you created above:
 # YOUR CODE HERE
 
-# Write code that prints all of the 'values' of the email hash 
+# Write code that prints all of the 'values' of the email hash
 # you created above:
 # YOUR CODE HERE
 
@@ -61,7 +62,7 @@ p zoo
 # Check out the following example of an array of Instagram posts:
 
 
-posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"]
+#posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"]
 
 
 # An Array of Hashes is probably more realistic. Hashes are a data type that's great
@@ -76,7 +77,7 @@ posts = [
     timestamp: "4:37 PM August 13, 2019",
     number_likes: 0,
     comments: []
-  }, 
+  },
   {
     image_src: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -90,12 +91,12 @@ p posts
 p posts[0]
 
 
-# The code snippet above shows an Array with 2 elements. Each element in the Array is a 
-# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK 
+# The code snippet above shows an Array with 2 elements. Each element in the Array is a
+# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK
 # a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 # good to have some exposure before going into Mod 1.
 
 
-# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same 
+# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same
 # key-value pairs you used in your email Hash above.
 # Then, print the email Array to the terminal.
