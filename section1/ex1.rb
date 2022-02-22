2138e8dfa32b1b352086f05c79d334f5c7ae2810
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-02-21 22:45:25 -0700

    Complete exercises 1 and 2 and checked off README.md
    Created files for each exercise 1 and 2.
    
    Complete exercises in Atom, then ran to terminal.
    
    Checked off exercises 1 and 2 in README.md file.

---
+++ section1/ex1.rb
@@ -0,0 +1,10 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this"
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts "Go to bed as soon as you finish at 11:00pm!"
+
+# Comments out a line so it does not show in output.
