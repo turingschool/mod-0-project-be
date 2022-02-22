a1a4ecf5c076cda102dd8040de0ac12f64dc1ea1
Shirley DeCesari <shirley.decesari@gmail.com>
2022-02-21 22:14:36 -0700

    Add Section 1

---
+++ section1/ex3.rb
@@ -0,0 +1,18 @@
+puts "I will now count my chickens:"
+# #{} this will do the math
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+puts "Now I will count the eggs:"
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 +6
+# thiw will answer the question as false
+puts "Is it true that 3 +2 < 5 - 7?"
+puts 3 + 2 < 5 - 7
+# "will show the math #{} will solve the math"
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+puts "Oh, that's why it's false."
+puts "How about some more."
+#Will solve the question
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -1}"
---
+++ section1/ex6.rb
@@ -0,0 +1,31 @@
+# setting up the variable for numbe of people
+types_of_people = 10
+# setting up the variable for x
+x = "There are #{types_of_people} types of people."
+# setting up the variable for binary
+binary = "binary"
+# setting up the variable for do not
+do_not = "don't"
+# setting up the variable for y
+y = "those who know #{binary} and those who #{do_not}"
+
+puts x
+puts y
+
+# using the x and y variable
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+# setting up the varialbe for false
+hilarious = false
+# using the false variable
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+# setting up the w and e variable to be added together
+w = "This is the left side of..."
+e = 'a string with a right side.'
+
+# using the w and e varaible to form one string by adding them together with +
+puts w + e
