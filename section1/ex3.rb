8ee4de3a6687a66d4b05f88c1a8328c97766e44c
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-02-22 16:18:18 -0700

    Rewrite ex3.rb to use floating point numbers for accuracy

--- section1/ex3.rb
+++ section1/ex3.rb
@@ -2,27 +2,27 @@
 puts "I will now count my chickens:"
 
 # Prints: "Hens" and the output of the calculation inside brackets
-puts "Hens #{25 + 30 / 6}"
+puts "Hens #{25.0 + 30.0 / 6.0}"
 # Prints "Roosters and the output of the calculation inside brackets"
-puts "Roosters #{100 - 25 * 3 % 4}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
 
 # Prints "Now I will count the eggs:"
 puts "Now I will count the eggs:"
 
 # Prints the output of the calculation
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 # prints "Is it true that" and the output of the calculation inside brackets followed by a "?"
-puts "Is it true that 3 + 2 < 5 - 7?"
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
 
 # Prints the output of the calculation
-puts 3 + 2 < 5 - 7
+puts 3.0 + 2.0 < 5.0 - 7.0
 
 # Prints "What is" out of calculation "?" output of calculation
-puts "what is 3 + 2? #{3 + 2}"
+puts "what is 3.0 + 2.0? #{3.0 + 2.0}"
 
 # Prints "What is" out of calculation "?" output of calculation
-puts "What is 5 - 7? #{5 - 7}"
+puts "What is 5.0 - 7.0? #{5.0 - 7.0}"
 
 # Prints "Oh, that's why it's false."
 puts "Oh, that's why it's false."
@@ -31,10 +31,10 @@ puts "Oh, that's why it's false."
 puts "How about some more."
 
 # Prints "Is it greater?" "true or fase"
-puts "Is it greater? #{5 > -2}"
+puts "Is it greater? #{5.0 > -2.0}"
 
 # Prints "Is it greater?" "true or fase"
-puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it greater or equal? #{5.0 >= -2.0}"
 
 # Prints "Is it greater?" "true or fase"
-puts "Is it less or equal? #{5 <= -2}"
+puts "Is it less or equal? #{5.0 <= -2.0}"
