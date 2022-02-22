15e05c39617ffe864d9b9cc46163d55482bde68f
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-02-22 15:57:39 -0700

    Add # before each line to determine output

--- section1/ex3.rb
+++ section1/ex3.rb
@@ -1,26 +1,40 @@
+# Prints "I will now count my chickens:"
 puts "I will now count my chickens:"
 
+# Prints: "Hens" and the output of the calculation inside brackets
 puts "Hens #{25 + 30 / 6}"
+# Prints "Roosters and the output of the calculation inside brackets"
 puts "Roosters #{100 - 25 * 3 % 4}"
 
+# Prints "Now I will count the eggs:"
 puts "Now I will count the eggs:"
 
+# Prints the output of the calculation
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# prints "Is it true that" and the output of the calculation inside brackets followed by a "?"
 puts "Is it true that 3 + 2 < 5 - 7?"
 
+# Prints the output of the calculation
 puts 3 + 2 < 5 - 7
 
+# Prints "What is" out of calculation "?" output of calculation
 puts "what is 3 + 2? #{3 + 2}"
 
+# Prints "What is" out of calculation "?" output of calculation
 puts "What is 5 - 7? #{5 - 7}"
 
+# Prints "Oh, that's why it's false."
 puts "Oh, that's why it's false."
 
+# Prints "How about some more."
 puts "How about some more."
 
+# Prints "Is it greater?" "true or fase"
 puts "Is it greater? #{5 > -2}"
 
+# Prints "Is it greater?" "true or fase"
 puts "Is it greater or equal? #{5 >= -2}"
 
+# Prints "Is it greater?" "true or fase"
 puts "Is it less or equal? #{5 <= -2}"
