66257fe800fc5c000e24945a1826684e177c3440
Colin Reinhart <colinbreinhartgmail.com>
2022-02-23 13:11:26 -0700

    Add Section 1

--- section1/exercises/variables.rb
+++ section1/exercises/variables.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section1/exercises/variables.rb`
 
 # Example: Write code that saves your name to a variable and
@@ -11,50 +11,61 @@ p name
 # Write code that saves the string 'Dobby' to a variable and
 # prints what that variable holds to the terminal:
 house_elf = "Dobby"
-# YOUR CODE HERE
+p house_elf
 
 # Write code that saves the string 'Harry Potter must not return to Hogwarts!'
 # and prints what that variable holds to the terminal:
-# YOUR CODE HERE
+p "#{name} must not return to Hogwarts!"# YOUR CODE HERE
 
 # Write code that adds 2 to the `students` variable and
 # prints the result:
 students = 22
-# YOUR CODE HERE
-p students
+p students + 2
+
 
 # Write code that subracts 2 from the `students` variable and
 # prints the result:
-# YOUR CODE HERE
-p students
+p students - 2
+
 
 
 # YOU DO:
-# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`. 
+# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`.
 # Store the appropriate data types in each.
 # print all three variables to the terminal.
 
+first_name = "Colin"
+is_hungry = false
+number_of_pets = 1
+p first_name
+p is_hungry
+p number_of_pets
+
 # IN WORDS:
-# How did you decide to use the data type you did for each of the three variables above? 
-
+# How did you decide to use the data type you did for each of the three variables above?
+p "The variable first_name is a string, is_hingy is a boolean, and the number of pets is an integer."
 # Explain.
-
+p "first_name could be any combination of letters, so it is a string. is_hungry is a true or false statement, so it is a boolean, and number_of_pets could be any whole number (I hope no one has a fraction of pets at home🤞), so it is an integer."
 
 # YOU DO:
 # Re-assign the values to the three variables from the previous challenge to
 # different values (but same data type).
 # Print all three variables to the terminal.
-
-
+first_name = "Burke"
+is_hungry = true
+number_of_pets = 6
+p first_name
+p is_hungry
+p number_of_pets
 # YOU DO:
 # Using the variables below, print the total number of snacks to the terminal:
 healthy_snacks = 6;
 junk_food_snacks = 8;
-
+p healthy_snacks + junk_food_snacks
 
 #-------------------
 # FINAL CHECK
 #-------------------
 
 # Did you run this file in your terminal to make sure everything printed out
-# to the terminal as you would expect? 
+# to the terminal as you would expect?
--- section1/reflection.md
+++ section1/reflection.md
@@ -2,18 +2,37 @@
 
 1. Regarding the blog posts in Part A, how do you feel about asking questions? Do you tend to ask them too soon, or wait too long, or somewhere in between?
 
+I usually wait too long. I enjoy the satisfaction of finding things out on my own and tend to work longer on my own before reaching out to ask questions.
+
 2. How would you print the string `"Hello World!"` to the terminal?
+
+print = "Hello World!"
 
 3. What character is used to indicate comments in a ruby file?
 
+.rb
+
 4. Explain the difference between an integer and a float?
+
+integers are whole numbers and floats have a decimal point (ex 1 is an integer and 0.1 is a float)
 
 5. In the space below, create a variable `animal` that holds the string `"zebra"`
 
+``animal = "zebra" ``
+
 6. How would you print the string `"zebra"` using the variable that you created above?
+
+``p animal ``
 
 7. What is interpolation? Use interpolation to print a sentence using the variable `animal`.
 
+``p "I saw a #{animal} at the zoo today."``
+
 8. What method is used to get input from a user?
 
+``gets.chomp``
+
 9. Name and describe two common string methods:
+
+``.capitolize`` - This will capitolize the first letter of the string and lower-case the rest of the letters
+``.reverse`` - Reverses the string.
