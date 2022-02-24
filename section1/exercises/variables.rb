2a08e54c0a92d34aca04a2cb97d7383b7b9fa03b
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-02-24 16:23:30 -0700

    Complete variables.rb

--- section1/exercises/variables.rb
+++ section1/exercises/variables.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section1/exercises/variables.rb`
 
 # Example: Write code that saves your name to a variable and
@@ -11,50 +11,62 @@ p name
 # Write code that saves the string 'Dobby' to a variable and
 # prints what that variable holds to the terminal:
 house_elf = "Dobby"
-# YOUR CODE HERE
+p house_elf
 
 # Write code that saves the string 'Harry Potter must not return to Hogwarts!'
 # and prints what that variable holds to the terminal:
-# YOUR CODE HERE
+comment = "Harry Potter must not return to Hogwarts!"
+p comment
 
 # Write code that adds 2 to the `students` variable and
 # prints the result:
 students = 22
-# YOUR CODE HERE
-p students
+puts "#{students + 2}"
 
 # Write code that subracts 2 from the `students` variable and
 # prints the result:
-# YOUR CODE HERE
-p students
+puts "#{students - 2}"
 
 
 # YOU DO:
-# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`. 
+# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`.
 # Store the appropriate data types in each.
 # print all three variables to the terminal.
+first_name = "Rue"
+is_hungry = "is hungry"
+number_of_pets = 0
+
+puts "#{first_name} #{is_hungry}. They have #{number_of_pets} pets."
 
 # IN WORDS:
-# How did you decide to use the data type you did for each of the three variables above? 
+# How did you decide to use the data type you did for each of the three variables above?
+
+# I used string interpolation to insert the 3 variables in the string.
 
 # Explain.
-
+# Plugged each variable inside #{} in the sentence borded by double quotes.
 
 # YOU DO:
 # Re-assign the values to the three variables from the previous challenge to
 # different values (but same data type).
 # Print all three variables to the terminal.
 
+first_name = "Debbie"
+is_hungry = "is hungry"
+number_of_pets = 1
+
+puts "#{first_name} #{is_hungry}. They have #{number_of_pets} pets."
 
 # YOU DO:
 # Using the variables below, print the total number of snacks to the terminal:
 healthy_snacks = 6;
 junk_food_snacks = 8;
 
-
+puts healthy_snacks + junk_food_snacks
 #-------------------
 # FINAL CHECK
 #-------------------
 
 # Did you run this file in your terminal to make sure everything printed out
-# to the terminal as you would expect? 
+# to the terminal as you would expect?
+# Yes
