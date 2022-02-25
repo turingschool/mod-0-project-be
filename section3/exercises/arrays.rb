cbf31aa5fe06cc5926c3880c09ecfd720f57f301
Colin Reinhart <colinbreinhartgmail.com>
2022-02-25 15:40:59 -0700

    Add section 3 exercises

--- section3/exercises/arrays.rb
+++ section3/exercises/arrays.rb
@@ -5,7 +5,7 @@
 # Two examples are already completed. Your task is to complete
 # any remaining prompt.
 
-# To check your work, run this file by entering the following 
+# To check your work, run this file by entering the following
 # command in your terminal:
 # `ruby section3/exercises/arrays.rb``
 
@@ -25,65 +25,67 @@ print animals[0]
 
 # YOU DO: Write code below that will print the number of elements in array of
 # animals from above.
-
+print animals.length
 
 # YOU DO: Write code that will reassign the last item in the animals
 # array to "Gorilla"
 
+p animals.map { |x| x == "Elephant" ? "Gorilla" : x }
+
 
 # YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
-
+p animals.map { |x| x == "Elephant" ? "Rubber Duck" : x }
+#
 # YOU DO: Write code that will print the String "Elephant" in the animals array
-
-
-#-------------------
+p animals[2]
+# -------------------
 # PART 2: Foods: Array Methods
-#-------------------
-
+# -------------------
+#
 # YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
-
+nom_nom = ["Duck Confit", "Peeking Duck", "Duck Fat Fries", "Duck a l'Orange"]
+#
 # YOU DO: Write code below that will print the number of elements in the array of
 # foods from above.
-
-
+print nom_nom.length
+#
 # YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 # print the changed array to verify "broccoli" has been added
-
-
+nom_nom << "broccoli"
+print nom_nom
+#
 # YOU DO: Write code below that removes the last item of food from the foods array and
 # print the changed array to verify that item has been removed
-
-
-# YOU DO: Write code to add 3 new foods to the array. 
-  # There are several ways to do this - choose whichever you'd like!
+nom_nom.pop
+print nom_nom
+# YOU DO: Write code to add 3 new foods to the array.
+#   There are several ways to do this - choose whichever you'd like!
 # Then, print the changed array to verify the new items have been added
-
+nom_nom.push("Tacos de Carnitas de Pato", "Duck Pate en Croute", "Roast Duck with Fig")
+print nom_nom
 # YOU DO: Remove the food that is in index position 0.
-
-#-------------------
+nom_nom.shift
+print nom_nom
+# -------------------
 # PART 3: Where are Arrays used?
-#-------------------
-
-
+# -------------------
+#
+#
 # Sometimes we need to hold on to multiple pieces of data, but have it grouped together in a list.
 # This is why programming languages have arrays!
-
+#
 # One example of a web/mobile application that uses arrays is Instagram. Each user has a set of posts
 # associated with their account. Each post, is one of potentially many, that are grouped together in a list, or, array.
-
+#
 # The post itself likely has more complex data, but here is one way we can think about it:
 
 
 posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"]
 
 # YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-# may be storing data? Come up with 3 examples - they could be from different web applications or 
+# may be storing data? Come up with 3 examples - they could be from different web applications or
 # all from the same one.
 
-# 1:
-# 2:
-# 3:
-
-
+# 1: Pintrist will have a list of things a user has pinned
+# 2: NYT Cooking will have a list of all my saved recipes
+# 3: Apple Maps will have a list of all the restaurants that serve duck in my general area.
---
+++ section3/exercises/ex2.rb
@@ -0,0 +1,18 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+ruby = animals[1]
+p "The animal at 1 is the #{ruby}."
+peacock = animals[2]
+p "The third animal is #{peacock}."
+bear = animals[0]
+p "The first animal is #{bear}."
+kangaroo = animals[3]
+p "The animal at 3 is the #{kangaroo}."
+whale = animals[4]
+p "The fifth animal is the #{whale}."
+at_2 = animals[2]
+p "The animal at 2 is the #{at_2}."
+sixth = animals[5]
+p "The sixth animal is the #{sixth}"
+at_4 = animals[4]
+p "The snimsl at 4 is the #{at_4}."
