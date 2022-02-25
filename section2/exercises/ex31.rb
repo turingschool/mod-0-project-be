99ccb54bb142600357bae5de8b7671181b9242ab
Jim Riddle <jimriddle0@gmail.com>
2022-02-25 10:02:26 -0600

    Add another game to ex31

--- section2/exercises/ex31.rb
+++ section2/exercises/ex31.rb
@@ -16,7 +16,7 @@ if door == "1"
   elsif bear == "2"
     puts "The bear eats your legs off. Good job!"
   else
-    puts "Well, doing %s is probably better. Bear runs away." 
+    puts "Well, doing %s is probably better. Bear runs away."
   end
 
 elsif door == "2"
@@ -35,4 +35,63 @@ elsif door == "2"
 
 else
   puts "you stumble around and fall on a knife and die.  Good job!"
+end
+
+
+#putting in a coin flipping game.  
+
+print "Choose between heads or tails "
+print "> "
+choice_1 = $stdin.gets.chomp
+#print choice_1
+#print choice_1 == "heads"
+
+if choice_1 == 'heads'
+  value_1 = 0
+elsif choice_1 == 'tails'
+  value_1 = 1
+else
+  value_1 = 'error'
+end
+
+#puts value_1
+
+coin_1 = rand(0..1)
+# for debugging purposes
+# puts coin_1
+
+if value_1 == 'error'
+  print "Come on, play the game!"
+elsif value_1 == coin_1
+  print "Nice, you win!"
+else
+  print "Do you want to double down? Y or N"
+  print "> "
+
+  choice_2 = $stdin.gets.chomp
+
+  if choice_2 == 'N'
+    print "Quit before you can lose more, I respect it"
+
+  elsif choice_2 == 'Y'
+    print "Here we go again! Choose bewteen heads or tails"
+    print "> "
+    choice_3 = gets.chomp
+
+    if choice_3 == 'heads'
+      value_2 = 0
+    elsif choice_1 == 'tails'
+      value_2 = 1
+    end
+
+    coin_2 = rand(0..1)
+
+    if value_2 == coin_2
+      print "Broke even, but just barely"
+    else
+      print "Now you are deep in the hole!"
+    end
+
+
+  end
 end
--- section2/exercises/ruby_additional_practice.rb
+++ section2/exercises/ruby_additional_practice.rb
@@ -1,11 +1,83 @@
-def water_status (minutes)
-  if minutes < 7
-    puts "The ater is not boiling yet."
-  elsif minutes == 7
-    puts "It's just barely boiling"
-  elsif minutes == 8
-    puts "It's boiling"
-  else
-    puts "Hot! Hot! Hot!"
+#def water_status (minutes)
+#  if minutes < 7
+#    puts "The ater is not boiling yet."
+#  elsif minutes == 7
+#    puts "It's just barely boiling"
+#  elsif minutes == 8
+#    puts "It's boiling"
+#  else
+#    puts "Hot! Hot! Hot!"
+#  end
+#end
+
+
+print "Choose between heads or tails "
+print "> "
+choice_1 = $stdin.gets.chomp
+#print choice_1
+#print choice_1 == "heads"
+
+if choice_1 == 'heads'
+  value_1 = 0
+elsif choice_1 == 'tails'
+  value_1 = 1
+else
+  value_1 = 'error'
+end
+
+#puts value_1
+
+coin_1 = rand(0..1)
+# for debugging purposes
+# puts coin_1
+
+if value_1 == 'error'
+  print "Come on, play the game!"
+elsif value_1 == coin_1
+  print "Nice, you win!"
+else
+  print "Do you want to double down? Y or N"
+  print "> "
+
+  choice_2 = $stdin.gets.chomp
+
+  if choice_2 == 'N'
+    print "Quit before you can lose more, I respect it"
+
+  elsif choice_2 == 'Y'
+    print "Here we go again! Choose bewteen heads or tails"
+    print "> "
+    choice_3 = gets.chomp
+
+    if choice_3 == 'heads'
+      value_2 = 0
+    elsif choice_1 == 'tails'
+      value_2 = 1
+    end
+
+    coin_2 = rand(0..1)
+
+    if value_2 == coin_2
+      print "Broke even, but just barely"
+    else
+      print "Now you are deep in the hole!"
+    end
+
+
   end
 end
+
+
+
+
+#puts rand(0..1)
+#puts rand(0..1)
+#puts rand(0..1)
+#puts rand(0..1)
+#puts rand(0..1)
+
+#puts rand(0..2)
+#puts rand(0..2)
+#puts rand(0..2)
+#puts rand(0..2)
+#puts rand(0..2)
