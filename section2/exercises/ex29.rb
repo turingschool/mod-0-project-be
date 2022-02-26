efa9117721c41db2669385540a34df73f08d323c
Shirley DeCesari <shirley.decesari@gmail.com>
2022-02-26 14:46:42 -0700

    Add section 2 work

---
+++ section2/exercises/ex29.rb
@@ -0,0 +1,45 @@
+people = 10
+cats = 30
+dogs = 35
+
+
+if people < cats
+ puts "Too many cats! The world is doomed!"
+end
+
+if people > cats
+ puts "Not many cats! The world is saved!"
+end
+
+if people < dogs
+puts "The world is drooled on!"
+end
+
+if people > dogs
+puts "The world is dry!"
+end
+
+
+dogs += 5
+
+if people >= dogs
+ puts "people are greater than or equal to dogs."
+end
+
+if people <= dogs
+puts "People are less than or equal to dogs."
+end
+
+
+if people == dogs
+puts "People are dogs."
+end
+
+
+
+# 1. if sets up if the statment will run
+# 2. according to google the code under the if needs to be indented to denote
+# the code block
+# 3. i tried to remove the indention but nothing changed but google says all
+# the codes will run and the output will be different
+# 5. changing the initial value changes the output
---
+++ section2/exercises/ex30.rb
@@ -0,0 +1,34 @@
+people = 10
+cars = 40
+trucks = 15
+
+# cars is greater than people "We should take the cars" will print
+# if both if statements were false then "We can't decide" will print
+if cars > people
+  puts "We should take the cars."
+elsif cars < people
+  puts "We should not take the cars."
+else
+  puts "We can't decide."
+end
+
+# trucks is less than cars "Maybe we could take the trucks" will prints
+# if trucks was greater than cars it would print
+# if both statements were false then "We still can't decide" would print
+if trucks > cars
+  puts "That's too many trucks."
+elsif trucks < cars
+  puts "Maybe we could take the trucks."
+else
+  puts "We still can't decide."
+end
+
+# trucks is greater than people "Fine, let's stay home then" will print
+if people > trucks
+  puts "Alright, let's just take the trucks."
+else
+  puts "Fine, let's stay home then."
+end
+
+# 1. elsif will print whichever statment is true
+# else will print the other statement if the first and or second is false
