8628fdc6cf7e699f1e5e3b889f3d7070fa15c677
Sarah Hall <swatson1994@yahoo.com>
2022-02-27 16:51:10 -0500

    Add array work

--- section3/exercises/hashes.rb
+++ section3/exercises/hashes.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section3/exercises/hashes.rb`
 
 # Example: Write code that prints a hash holding grocery store inventory:
@@ -8,25 +8,25 @@ foods = {apples: 23, grapes: 507, eggs: 48}
 p foods
 
 # Write code that prints a hash holding zoo animal inventory:
-zoo = #YOUR CODE HERE
+zoo = { "zebras" => 5, "elephants" => 4, "eagles" => 7, "penguins" => 3}
 p zoo
 
-# Write code that prints all of the 'keys' of the zoo variable 
+# Write code that prints all of the 'keys' of the zoo variable
 # you created above:
-# YOUR CODE HERE
+p zoo.keys
 
-# Write code that prints all of the 'values' of the zoo variable 
+# Write code that prints all of the 'values' of the zoo variable
 # you created above:
-# YOUR CODE HERE
+p zoo.values
 
-# Write code that prints the value of the first animal of the zoo variable 
+# Write code that prints the value of the first animal of the zoo variable
 # you created above:
-# YOUR CODE HERE
+p zoo["zebras"]
 
-# Write code that adds an animal to the zoo hash. 
+# Write code that adds an animal to the zoo hash.
 # Then, print the updated hash:
-# YOUR CODE HERE
-
+zoo["lemurs"] = 5
+p zoo
 
 #-------------------
 # Part 2: Email
@@ -40,15 +40,23 @@ p zoo
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
 
 # Write code that prints your email hash to the terminal.
+email_info = {
+  "recipient" => "Dana Schmoe",
+  "CC'd" => "Dana's boss",
+  subject: "Late delivery",
+  greeting: "Good evening, Dana",
+  body: "It seems our shipment of milk is late again. Please ensure we get the milk before it expires.",
+  closing: "Do not fail us.",
+  signed: "The Calcium Bros"
+}
 
-
-# Write code that prints all of the 'keys' of the email hash 
+# Write code that prints all of the 'keys' of the email hash
 # you created above:
-# YOUR CODE HERE
+p email_info.keys
 
-# Write code that prints all of the 'values' of the email hash 
+# Write code that prints all of the 'values' of the email hash
 # you created above:
-# YOUR CODE HERE
+p email_info.values
 
 
 #-------------------
@@ -76,7 +84,7 @@ posts = [
     timestamp: "4:37 PM August 13, 2019",
     number_likes: 0,
     comments: []
-  }, 
+  },
   {
     image_src: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -90,12 +98,43 @@ p posts
 p posts[0]
 
 
-# The code snippet above shows an Array with 2 elements. Each element in the Array is a 
-# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK 
+# The code snippet above shows an Array with 2 elements. Each element in the Array is a
+# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK
 # a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 # good to have some exposure before going into Mod 1.
 
 
-# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same 
+# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same
 # key-value pairs you used in your email Hash above.
 # Then, print the email Array to the terminal.
+outbox = [
+{
+  "recipient" => "Dana Schmoe",
+  "CC'd" => "Dana's boss",
+  subject: "Late delivery",
+  greeting: "Good evening, Dana",
+  body: "It seems our shipment of milk is late again. Please ensure we get the milk before it expires.",
+  closing: "Do not fail us.",
+  signed: "The Calcium Bros"
+},
+{
+  "recipient" => "Mama",
+  "CC'd" => "Papa",
+  subject: "still no milk",
+  greeting: "Hi mama,",
+  body: "I'm sorry to say, but we don't have any milk yet. I promise to aquire enough milk for your cereal soon.",
+  closing: "I'm so sorry.",
+  signed: "Your loving son, Denny."
+},
+{
+  "recipient" => "Grandma Joanna",
+  "CC'd" => "N/A",
+  subject: "Hope you're happy",
+  greeting: "Salutations, milk glutton.",
+  body: "I hope you're pleased with yourself, there's no milk to be found in our town. Curse your over-calcified, crusty bones.",
+  closing: "Others need milk too.",
+  signed: "Your withering grandson, Dennison."
+}
+]
+
+p outbox
