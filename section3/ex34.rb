44ebd5eea99c99819b8b3c13a2e68e1c5cb23e35
Jim Riddle <jimriddle0@gmail.com>
2022-02-28 17:16:57 -0600

    Add 3 exercises

---
+++ section3/ex34.rb
@@ -0,0 +1,18 @@
+animal = ['bear', 'ruby', 'peakcock', 'kangaroo', 'whale', 'platypus']
+
+puts "The animal at 1 is #{animal[1]}"
+puts "The third animal is #{animal[2]}"
+puts "The first animal is #{animal[0]}"
+puts "The animal at 3 is #{animal[3]}"
+puts "The fifth animal is #{animal[4]}"
+puts "The animal at 2 is #{animal[2]}"
+puts "The sixth animal is #{animal[5]}"
+puts "The animal at 4 is #{animal[4]}"
+
+# Study Drill 1 - The reason that "January 1, 2010" is in 2010 and not 2009 is that when the array of days in 2009 was over, it went to 2010
+# If i'm going to be honest, i have no idea what this question is asking
+
+names = ['peter', 'frank', 'william']
+
+puts "The first name is #{names[0]}"
+puts "The name at 2 is #{names[2]}"
