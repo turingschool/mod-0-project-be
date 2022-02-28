37f524e1f3e989233700e47b3dd0d1630f061acd
Wyatt Kribs <kribswyatt@gmail.com>
2022-02-28 13:21:48 -0700

    added ex5

---
+++ section1/learn_the_hard_way/ex5.rb
@@ -0,0 +1,20 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "lets talk about #{name}"
+puts "he's #{height} inches tall"
+puts "he's #{weight} pounds heavy"
+puts "actually that's not too heavy"
+puts "he's got #{eyes} eyes and #{hair} hair"
+puts "his teeth are usually #{teeth} depending on the coffee"
+
+puts "if I add #{age}, #{height}, and #{weight} I get #{age + height + weight}"
+
+#metric conversions
+height_metric = 74.0 * 2.54
+weight_metric = 180.0 * 0.45
