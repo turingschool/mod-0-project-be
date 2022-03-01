130ba0de765e64b16fcedfd4c877cd13be271cc4
Shirley DeCesari <shirley.decesari@gmail.com>
2022-02-28 22:51:04 -0800

    Add exercises

---
+++ section3/exercises/ex32.rb
@@ -0,0 +1,36 @@
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+# this first kind of for-loop goes througha list
+# in a more traditinoal style found in other languages
+the_count.each do |number|
+  puts "This is count #{number}"
+end
+
+# same as above, but in a more Ruby style
+# this and the next one are the preferred
+# way Ruby for-loops are written
+fruits.each do |fruit|
+  puts "A fruit of type: #{fruit}"
+end
+
+# also we can go through mixed lists # too
+# not this is yet another sytle, exactly like aboce
+# but a different syntax (way to write it).
+change.each {|i| puts "I got #{i}"}
+
+# we can also build lists, first start with an empty one
+elements = []
+
+# then use the range operator do do 0 to 5 counts
+(0..5).each do |i|
+  puts "adding #{i} to the list."
+  # pushes the i variable on the *end* of the lists
+  elements.push(i)
+end
+
+# now we cna print them out # too
+elements.each {|i| puts "Element was: #{i}"}
+
+# 1. the range operator creates a sequence between two values (a..z) or (1..50)
---
+++ section3/exercises/ex34.rb
@@ -0,0 +1,19 @@
+animals = ['bear', 'ruby', 'peackock', 'kangaroo', 'whale', 'platypus']
+# 1. The animal at 1.
+print animals[1]
+# 2. The third (3rd) animal.
+print animals[2]
+# 3. The first (1st) animal.
+print animals[0]
+# 4. The animal at 3.
+print animals[3]
+# 5. The fifth (5th) animal.
+print animals[4]
+# 6. The animal at 2.
+print animals[2]
+# 7. The sixth (6th) animal.
+print animals[5]
+# 8. The animal at 4.
+print animals[4]
+
+# 1. the year is a cardinal number
