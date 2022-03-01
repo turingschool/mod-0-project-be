5be8ea84613e9734e72bcd9ddf25cc466e5cbcef
Jim Riddle <99755958+jimriddle1@users.noreply.github.com>
2022-03-01 15:02:43 -0600

    Clean up syntax

--- section1/ex5.rb
+++ section1/ex5.rb
@@ -13,7 +13,7 @@ puts "He's #{height} inches tall."
 puts "He's #{weight} pounds heavy."
 puts "Actually that's not too heavy."
 puts "He's got #{eyes} eyes and #{hair}."
-puts "his teeth are usually #{teeth} depending on the coffee."
+puts "His teeth are usually #{teeth} depending on the coffee."
 puts "He weighs #{weight_kilo} kilograms"
 puts "He is #{height_cm} centimeters tall"
 
