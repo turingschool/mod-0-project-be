ad0fa5c689f5527e4c220bc6669b23a72f9de1b6
Chris Lyman <christopher.lyman0@gmail.com>
2022-03-08 18:22:40 -0700

    Created exercise

---
+++ section1/ex6.rb
@@ -0,0 +1,29 @@
+# create a variable named "types_of _people and setting it = to 10"
+types_of_people = 10
+# creating a variable named "x" and setting it equal to a string and insrting variable "types_of_peop;e"
+x = "There are #{types_of_people} types of people"
+# creating a variable "binary" and setting it equal to binary
+binary = "binary"
+# creating a variable "do_not" and setting it equal to don't
+do_not = "don't"
+# inserting the variables "binary" & "do_not" into a string
+y = "Those who know #{binary} and those who #{do_not} ."
+# printing the variables "x" & "y" to the screen
+puts x
+puts y
+# inserting the variable "x" into a string and printing it to the screen
+puts "I said: #{x} ."
+# inserting the variable "y" into a string and printing it to the screen
+puts "I also said #{y} ."
+# creating the variable "hilarious" and setting it equal to false
+hilarious = false
+# careating the variable "joke_evaluation" and setting it eaual to a string and inserting the variable "hilarious"
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+# prints the variable "joke_evaluation" to the screen
+puts joke_evaluation
+# creates the variable "w" and sets it equal to "This is the left side of..."
+w = "This is the left side of..."
+# creates the vriable "e" and sets it equal to s string with a right side.
+e = "a string with a right side."
+# combines the two variables "w" & "e" and prints the result to the screen
+puts w + e
