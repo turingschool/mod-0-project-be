3205700e57b3fc227bffc652dd540aed69250190
Chris Lyman <christopher.lyman0@gmail.com>
2022-03-08 18:35:11 -0700

    Added code answers

--- section1/exercises/variables.rb
+++ section1/exercises/variables.rb
@@ -1,60 +1,74 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section1/exercises/variables.rb`
 
 # Example: Write code that saves your name to a variable and
 # prints what that variable holds to the terminal:
-name = "Harry Potter"
+name = "Chris Lyman"
 p name
 
 # Write code that saves the string 'Dobby' to a variable and
 # prints what that variable holds to the terminal:
 house_elf = "Dobby"
-# YOUR CODE HERE
+p house_elf
 
 # Write code that saves the string 'Harry Potter must not return to Hogwarts!'
 # and prints what that variable holds to the terminal:
-# YOUR CODE HERE
+not_return = "Harry Potter must not return to Hogwarts!"
+p not_return
 
 # Write code that adds 2 to the `students` variable and
 # prints the result:
 students = 22
-# YOUR CODE HERE
+students = students + 2
 p students
 
 # Write code that subracts 2 from the `students` variable and
 # prints the result:
-# YOUR CODE HERE
+students = students - 2
 p students
 
 
 # YOU DO:
-# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`. 
+# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`.
 # Store the appropriate data types in each.
 # print all three variables to the terminal.
+first_name = "Chris"
+is_hungry = "No"
+number_of_pets = 4
+p first_name
+p is_hungry
+p number_of_pets
 
 # IN WORDS:
-# How did you decide to use the data type you did for each of the three variables above? 
+# How did you decide to use the data type you did for each of the three variables above?
 
 # Explain.
+# I decided to use string for the first two variables as the answers were likly to be strings
+# and an integer for the lst one as it was likly to be an integer.
 
 
 # YOU DO:
 # Re-assign the values to the three variables from the previous challenge to
 # different values (but same data type).
 # Print all three variables to the terminal.
-
+first_name = "Lyman"
+is_hungry = "Maybe"
+number_of_pets = 1
+p first_name
+p is_hungry
+p number_of_pets
 
 # YOU DO:
 # Using the variables below, print the total number of snacks to the terminal:
 healthy_snacks = 6;
 junk_food_snacks = 8;
-
+p healthy_snacks + junk_food_snacks
 
 #-------------------
 # FINAL CHECK
 #-------------------
 
 # Did you run this file in your terminal to make sure everything printed out
-# to the terminal as you would expect? 
+# to the terminal as you would expect?
