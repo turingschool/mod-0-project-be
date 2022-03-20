7ff84e4e7a5d997002bc04fcf3a09e98679f0f4e
John Santosuosso <john.santosuosso1@gmail.com>
2022-03-20 09:37:10 -0400

    arrays.rb

--- section3/exercises/arrays.rb
+++ section3/exercises/arrays.rb
@@ -5,7 +5,7 @@
 # Two examples are already completed. Your task is to complete
 # any remaining prompt.
 
-# To check your work, run this file by entering the following 
+# To check your work, run this file by entering the following
 # command in your terminal:
 # `ruby section3/exercises/arrays.rb``
 
@@ -17,51 +17,57 @@
 # EXAMPLE: write code below that will print an array of animals.
 # Store the array in a variable.
 animals = ["Zebra", "Giraffe", "Elephant"]
-print animals
+puts animals
 
 # EXAMPLE: Write code below that will print "Zebra" from the animals array
 # YOUR CODE HERE
-print animals[0]
+puts animals[0]
 
 # YOU DO: Write code below that will print the number of elements in array of
 # animals from above.
-
+puts "There are #{animals.count} animals in this array."
 
 # YOU DO: Write code that will reassign the last item in the animals
 # array to "Gorilla"
-
+animals[2]="Gorilla"
 
 # YOU DO: Write code that will add a new animal (type of your choice) to position 3.
+animals.push "Ardvark"
 
 
 # YOU DO: Write code that will print the String "Elephant" in the animals array
-
+animals.push "Elephant"
 
 #-------------------
 # PART 2: Foods: Array Methods
 #-------------------
 
 # YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+foods = ['apples', 'peaches', 'plums', 'cherries']
 
 # YOU DO: Write code below that will print the number of elements in the array of
 # foods from above.
-
+puts "There are #{foods.count} foods in the array"
 
 # YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 # print the changed array to verify "broccoli" has been added
-
-
+foods.push "broccoli"
+puts foods
 # YOU DO: Write code below that removes the last item of food from the foods array and
 # print the changed array to verify that item has been removed
-
-
-# YOU DO: Write code to add 3 new foods to the array. 
+foods.pop
+puts foods
+# YOU DO: Write code to add 3 new foods to the array.
   # There are several ways to do this - choose whichever you'd like!
+new_foods = ['corn', 'cornbeef', 'potatoes']
+foods.push new_foods
+foods.flatten
+
 # Then, print the changed array to verify the new items have been added
-
+puts foods
 # YOU DO: Remove the food that is in index position 0.
-
+foods.shift
+puts foods
 #-------------------
 # PART 3: Where are Arrays used?
 #-------------------
@@ -79,11 +85,9 @@ print animals[0]
 posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"]
 
 # YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-# may be storing data? Come up with 3 examples - they could be from different web applications or 
+# may be storing data? Come up with 3 examples - they could be from different web applications or
 # all from the same one.
 
-# 1:
-# 2:
-# 3:
-
-
+# 1:  Spotify--using lists to store songs?
+# 2:  Lyft-using lists to keep track of ride history
+# 3:  DoorDash-using lists to output order
