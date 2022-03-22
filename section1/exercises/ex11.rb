e3e61843e75ba65ed7360d21e29530b0864827f4
Kevin Ta <kvnta@yahoo.com>
2022-03-22 02:20:33 -0600

    Initial commit

---
+++ section1/exercises/ex11.rb
@@ -0,0 +1,19 @@
+print "How old are you?"
+age = gets.chomp
+print "how tall are you?"
+height = gets.chomp
+print "How much do you weigh?"
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+# Study Drills
+
+# gets.chomp is where user would input their answer
+
+print "What's your favorite kind of dog?"
+dog = gets.chomp
+print "Is it a small dog or big dog?"
+size = gets.chomp
+
+puts " So your favorite dog is #{dog} and it is a #{size}. "
