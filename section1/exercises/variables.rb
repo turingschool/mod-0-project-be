f015e9e2456bfefc85aeaa8d929f043d9bd090be
Kevin Ta <kvnta@yahoo.com>
2022-03-22 09:35:19 -0600

    Complete variables exercise

--- section1/exercises/variables.rb
+++ section1/exercises/variables.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section1/exercises/variables.rb`
 
 # Example: Write code that saves your name to a variable and
@@ -11,50 +11,64 @@ p name
 # Write code that saves the string 'Dobby' to a variable and
 # prints what that variable holds to the terminal:
 house_elf = "Dobby"
-# YOUR CODE HERE
+
 
 # Write code that saves the string 'Harry Potter must not return to Hogwarts!'
 # and prints what that variable holds to the terminal:
-# YOUR CODE HERE
+snape = "Harry Potter must not return to Hogwarts!"
 
 # Write code that adds 2 to the `students` variable and
 # prints the result:
 students = 22
 # YOUR CODE HERE
-p students
+p students + 2
 
 # Write code that subracts 2 from the `students` variable and
 # prints the result:
 # YOUR CODE HERE
-p students
+p students - 2
 
 
 # YOU DO:
-# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`. 
+# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`.
 # Store the appropriate data types in each.
 # print all three variables to the terminal.
+first_name = "Kevin"
+is_hungry = "always"
+number_of_pets = "one"
+
+puts "The student #{first_name} has a big appetite #{is_hungry} and has #{number_of_pets} dog."
+
 
 # IN WORDS:
-# How did you decide to use the data type you did for each of the three variables above? 
+# How did you decide to use the data type you did for each of the three variables above?
 
+#
 # Explain.
-
+# I used the data type that could represent the variables related to me and that would make sense.
 
 # YOU DO:
 # Re-assign the values to the three variables from the previous challenge to
 # different values (but same data type).
 # Print all three variables to the terminal.
+Kevin = 'male'
+always = 'all day'
+one = "corgi"
 
+puts "The #{Kevin} student seems to eat #{always} with his pet #{one}."
 
 # YOU DO:
 # Using the variables below, print the total number of snacks to the terminal:
 healthy_snacks = 6;
 junk_food_snacks = 8;
 
+p "#{healthy_snacks + junk_food_snacks}"
 
 #-------------------
 # FINAL CHECK
 #-------------------
 
 # Did you run this file in your terminal to make sure everything printed out
-# to the terminal as you would expect? 
+# to the terminal as you would expect?
+
+# Everything came out correct with no errors.
