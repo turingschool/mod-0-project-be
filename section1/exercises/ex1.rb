93b981b8b8e2281a76bdd8f77647be2f022d8947
Kevin Ta <kvnta@yahoo.com>
2022-03-22 00:52:52 -0600

    initial commit

---
+++ section1/exercises/ex1.rb
@@ -0,0 +1,13 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+
+STUDY DRILL
+1)
+puts "My dog's name is 'Ruby.'"
+
+2) # This is negating a line, using an octothorpe. 
---
+++ section1/exercises/ex3.rb
@@ -0,0 +1,31 @@
+# print text
+puts "I will now count my chickens:"
+# print text and number of Hens with computation string
+puts "Hens #{25.0 + 30.0 / 6.0}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+# print text
+puts "Now I will count the eggs:"
+# computation equation will be print the answer
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+# print text string
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+#computational equation will print the answer as true or false
+puts 3.0 + 2.0 < 5.0 - 7.0
+# print text and computation equation with answer
+puts "What is 3 + 2? #{3.0 + 2.0}"
+puts "What is 5 - 7? #{5.0 - 7.0}"
+# print text string
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+# print text and computation equation with answer as true or false
+puts "is it greater? #{5.0 > -2.0}"
+puts "is it greater or equal? #{5.0 >= -2.0}"
+puts "is it less or equal? #{5.0 <= -2.0}"
+
+STUDY DRILLS
+#octothrope lines above
+
+puts "I ate 4 apples out of 5 how many do I have left? #{5.0 - 1.0}"
