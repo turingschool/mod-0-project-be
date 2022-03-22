7f77cd4332013073df8d460eb0a4433c366f1fb6
Kevin Ta <kvnta@yahoo.com>
2022-03-22 01:15:16 -0600

    Initial commit

---
+++ section1/exercises/ex4.rb
@@ -0,0 +1,32 @@
+# the text variables are being assigned numberical values
+cars = 100
+space_in_car = 4.0
+drivers = 30
+passengers = 90
+# Text variable is given a value from the other text variables in an equation
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_car
+average_passengers_per_car = passengers / cars_not_driven
+
+
+puts "There are  #{cars} cars available."
+puts "There are only #{drivers} drivers available."
+puts "there will be #{cars_not_driven} empty cars today."
+puts "We can transport #{carpool_capacity} people today."
+puts "We have #{passengers} to carpool today."
+puts "We need to put about #{average_passengers_per_car} in each car."
+
+# STUDY DRILLS
+
+# The instructors error occured because he initially didn't define what "carpool_capacity" was or how to achieve the value.
+
+#1)
+# Using a floating number was not necessary for this exercise.
+
+#2)
+ a = 5
+ b = 2
+
+ c = a - b
+ puts "5-2= #{c}"
