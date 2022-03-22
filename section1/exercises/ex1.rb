e17820f9090dbbd4b38aa7580c815bab67023481
Kevin Ta <kvnta@yahoo.com>
2022-03-22 01:18:11 -0600

    Fix study drill

--- section1/exercises/ex1.rb
+++ section1/exercises/ex1.rb
@@ -6,8 +6,8 @@ puts "Yay! Printing."
 puts "I'd much rather you 'not'."
 puts 'I "said" do not touch this.'
 
-STUDY DRILL
-1)
+#STUDY DRILL
+#1)
 puts "My dog's name is 'Ruby.'"
 
-2) # This is negating a line, using an octothorpe. 
+#2) # This is negating a line, using an octothorpe. 
--- section1/exercises/ex2.rb
+++ section1/exercises/ex2.rb
@@ -8,6 +8,6 @@ puts "I could have code like this." # and the comment after is ignored
 
 puts "This will run."
 
-STUDY DRILLS
+#STUDY DRILLS
 
-N/A 
+#N/A 
