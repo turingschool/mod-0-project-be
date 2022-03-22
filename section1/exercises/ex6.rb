84faf067bf5d2febd7a2237c4a33606eec4062ec
Kevin Ta <kvnta@yahoo.com>
2022-03-22 02:01:24 -0600

    Initial commit

---
+++ section1/exercises/ex6.rb
@@ -0,0 +1,33 @@
+#variables are being defined as numeric or text
+types_of_people = 10
+x = "There are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+#print value of "x and y"
+puts x
+puts y
+
+#print value of variable x and y in sentences
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+# text variables are being defined
+hilarious = false
+# text variable defined as a string with a variable value "hilarious" in string
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# print defined value of "joke_evaluation"
+puts joke_evaluation
+
+# w & e assigned string values
+w = "This is the left side of ..."
+e = "a string with a right side."
+
+
+# print string values of w + e together to form a sentence.
+puts w + e
+
+# Study Drills
+#done
