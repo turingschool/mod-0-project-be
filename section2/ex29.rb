98268c4d22dacb5713c19c7fef9ceb7ecf000c9f
Kevin Ta <kvnta@yahoo.com>
2022-03-22 10:18:08 -0600

    Initial commit

---
+++ section2/ex29.rb
@@ -0,0 +1,50 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats
+  puts "Too many cats! The world is doomed!"
+end
+
+if people >cats
+  puts "Not  many cats! The world is saved!"
+end
+
+if people <dogs
+  puts "The world is drooled on!"
+end
+
+if people > dogs
+  puts "The world is dry!"
+end
+
+
+
+dogs += 5
+
+if people >= dogs
+  puts "People are greater than or equal to dogs."
+end
+
+if people <= dogs
+  puts "People are less than or equal to dogs."
+end
+
+
+if people == dogs
+  puts "People are dogs."
+end
+
+#Study Drill
+#1)
+# The "if" code is a condition that must be met in order to print the given string value.
+
+#2)
+#The code under "if" has two spaces indented for cleanliness/ organization?
+
+#3)
+# Nothing happens when the ruby file is ran. It remains the same.
+
+#4) EX 27 was not in our lesson plan
+
+#25) If the intial values were changeed it could give different printed or ommited results.
