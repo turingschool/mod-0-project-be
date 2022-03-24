ed298907fd9a7e87a0548f15c96ff325d122b244
Kevin Ta <kvnta@yahoo.com>
2022-03-23 23:36:10 -0600

    Initial commit

---
+++ section2/exercises/exconditionals.rb
@@ -0,0 +1,14 @@
+#Following along with the example in Ruby in 100 minutes
+#Conditinal branching/ Instructions
+
+def water_status(minutes)
+  if minutes < 7
+    puts "The water is not boiling yet."
+  elsif minutes == 7
+    puts "It's just barely boiling"
+  elsif minutes == 8
+    puts "It's boiling!"
+  else
+    puts "Hot! Hot! Hot!"
+  end
+end
