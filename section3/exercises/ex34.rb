bec25cc97f702804751d3d24e276f184e2386313
Kevin Ta <kvnta@yahoo.com>
2022-03-24 16:07:42 -0600

    Initial commit

---
+++ section3/exercises/ex34.rb
@@ -0,0 +1,28 @@
+
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+#1 The animal at 1 is second is a ruby. The second animal at 1 is a ruby.
+#2 The third (3rd) animal at 2 is a peacock. The animal at 2 is the third and is a peacock.
+#3 The first (1st) animal is at zero and is a bear. The animal at zero is the first and is a bear.
+#4 The animal at 3 is the fourth animal and is a kangaroo. The fourth animal is at 3 and is a kangaroo.
+#5 The fifth (5th) animal is at six and is a whale. The animal at six is the fifth animal and is a whale.
+#6 The animal at 2 is the third animal and is a peacock. The third animal is at 2 and is a peacock.
+#7 The sixth (6th) animal is at 5 and is a platypus. The animal at 5 is the sixth animal and is a platypus.
+#8 The animal at 4 is the fifth animal and is a whale. The fifth animal is at 4 and is a whale.
+
+#Study DRILL
+#1 There is no year 0 to start the year so it would be 2010 not 2009.
+
+#2
+
+mood = ['happy', 'sad', 'mad', 'hungry', 'sleepy' ]
+
+happy = mood[0]
+hungry = mood[3]
+sleepy = mood[4]
+sad = mood[1]
+mad = mood[2]
+mood.each do |mood|
+
+puts "I am so #{mood}"
+end
