307b0112ae62da64e4ca2ef65ef62436cab7622e
Chris Lyman <christopher.lyman0@gmail.com>
2022-03-25 19:51:42 -0600

    Refactor exercise

--- section1/ex5.rb
+++ section1/ex5.rb
@@ -1,5 +1,5 @@
 name = "Zed A. Shaw"
-age = 35 #not a lie in 2009
+age = 35 # not a lie in 2009
 height = 74 # inches
 weight = 180 # lbs
 eyes = "Blue"
@@ -15,3 +15,14 @@ puts "His teeth are usually #{teeth} depending on the coffee."
 
 # this line is trickey, try to get it exactly right
 puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}"
+
+# Study Drill
+# Convert inches to centimeters
+inches = 12
+centimeters = inches * 2.54
+puts "#{centimeters} centimeters"
+
+#Convert pounds to kilograms
+pounds = 5
+kilograms = pounds * 0.453
+puts "#{kilograms} kilograms"
--- section1/ex7.rb
+++ section1/ex7.rb
@@ -10,8 +10,16 @@ puts "So you're #{age} old, #{height} tall and #{weight} heavy."
 
 
 print "Input the first number."
+
+#inputs a string and converts it to an integer
 first_number  = gets.chomp.to_i
+
 print "Input the second number."
-second_number = gets.chomp.to_i
-total = first_number / second_number.to_f
+
+#inputs a number and converts it to a flost
+second_number = gets.chomp.to_f
+
+#Divides the first number by the second number
+total = first_number / second_number
+
 puts "The first number divided by the second number equals #{total}"
