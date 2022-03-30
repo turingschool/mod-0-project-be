cd23b3fb050b082ee139deac8f68a3e1f219b034
Nick J <nickjones9605@gmail.com>
2022-03-30 08:11:44 -0600

    initial commit add section B answers and excersises

---
+++ section3/exercises/hashes_ruby_100_minutes.rb
@@ -0,0 +1,13 @@
+#Hashes represent a collection of data where each element is addressed by name and assigned a value pair
+
+
+	
+
+produce = {"apples" => 3, "oranges" => 55, "carrots" => 12}
+
+puts "There are #{produce['oranges']} oranges in the fridge."
+
+produce["grapes"] = 221
+
+puts "There are #{produce['grapes']} grapes in the fridge."
+
