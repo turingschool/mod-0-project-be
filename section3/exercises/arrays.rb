0100e5196c9e7c4a9ef39d7ee748992fe53d46c0
Nick J <nickjones9605@gmail.com>
2022-03-30 08:44:29 -0600

    add arrays.rb excercise answers

--- section3/exercises/arrays.rb
+++ section3/exercises/arrays.rb
@@ -25,42 +25,51 @@ print animals[0]
 
 # YOU DO: Write code below that will print the number of elements in array of
 # animals from above.
-
+puts animals.length
 
 # YOU DO: Write code that will reassign the last item in the animals
 # array to "Gorilla"
-
+animals[-1] = "Gorilla"
 
 # YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+animals[-1] = "Penguin"
 
 # YOU DO: Write code that will print the String "Elephant" in the animals array
-
+#change the last element back to elephant
+animals[-1] = "Elephant"
+puts animals[-1]
 
 #-------------------
 # PART 2: Foods: Array Methods
 #-------------------
 
 # YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+foods = ['apples', 'pizza', 'chicken tenders', 'sushi']
 
 # YOU DO: Write code below that will print the number of elements in the array of
 # foods from above.
-
+foods.length
 
 # YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 # print the changed array to verify "broccoli" has been added
-
+foods << "Broccoli"
+puts foods
 
 # YOU DO: Write code below that removes the last item of food from the foods array and
 # print the changed array to verify that item has been removed
-
+foods.pop
+puts foods
 
 # YOU DO: Write code to add 3 new foods to the array. 
   # There are several ways to do this - choose whichever you'd like!
 # Then, print the changed array to verify the new items have been added
 
+foods << "Ice Cream"
+foods << "Bagels"
+foods << "Pasta"
+puts foods
 # YOU DO: Remove the food that is in index position 0.
+foods.shift
 
 #-------------------
 # PART 3: Where are Arrays used?
@@ -82,8 +91,9 @@ posts = ["image at beach", "holiday party", "adorable puppy", "video of cute bab
 # may be storing data? Come up with 3 examples - they could be from different web applications or 
 # all from the same one.
 
-# 1:
-# 2:
-# 3:
+# Steam
+# 1: games list 
+# 2: friends list
+# 3: wishlist
 
 
