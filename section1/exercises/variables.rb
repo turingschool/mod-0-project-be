fc71afbdb2a2f86ac6adeb6d3284544b5718eed0
Ethan Nguyen <etnguyen16@gmail.com>
2022-04-13 15:03:35 -0600

    Add section1

--- section1/exercises/variables.rb
+++ section1/exercises/variables.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section1/exercises/variables.rb`
 
 # Example: Write code that saves your name to a variable and
@@ -11,32 +11,37 @@ p name
 # Write code that saves the string 'Dobby' to a variable and
 # prints what that variable holds to the terminal:
 house_elf = "Dobby"
-# YOUR CODE HERE
+p house_elf
 
 # Write code that saves the string 'Harry Potter must not return to Hogwarts!'
 # and prints what that variable holds to the terminal:
-# YOUR CODE HERE
-
+Harry_Potter = "Harry Potter must not return to Hogwarts!"
+p Harry_Potter
 # Write code that adds 2 to the `students` variable and
 # prints the result:
 students = 22
-# YOUR CODE HERE
+p students + 2
 p students
 
 # Write code that subracts 2 from the `students` variable and
 # prints the result:
-# YOUR CODE HERE
+p students - 2
 p students
 
 
 # YOU DO:
-# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`. 
+# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`.
 # Store the appropriate data types in each.
 # print all three variables to the terminal.
-
+first_name = "Ethan"
+is_hungry = "Ethan"
+number_of_pets = 1
+p "first name is #{first_name}"
+p "#{is_hungry} is hungry"
+p "#{first_name} has #{number_of_pets} pet"
 # IN WORDS:
-# How did you decide to use the data type you did for each of the three variables above? 
-
+# How did you decide to use the data type you did for each of the three variables above?
+p "To be honest, I wasn't sure if it wanted something related to the previous data, so I just used numbers that applied to me personally."
 # Explain.
 
 
@@ -44,17 +49,23 @@ p students
 # Re-assign the values to the three variables from the previous challenge to
 # different values (but same data type).
 # Print all three variables to the terminal.
-
+first_name = "Harry"
+is_hungry = "Harry"
+number_of_pets = 1
+p "his first name is #{first_name}"
+p "#{is_hungry} is hungry"
+p "#{first_name} has #{number_of_pets} pet"
 
 # YOU DO:
 # Using the variables below, print the total number of snacks to the terminal:
 healthy_snacks = 6;
 junk_food_snacks = 8;
-
+p "total of healthy snacks is #{healthy_snacks}"
+p "total of junk food snacks is #{junk_food_snacks}"
 
 #-------------------
 # FINAL CHECK
 #-------------------
 
 # Did you run this file in your terminal to make sure everything printed out
-# to the terminal as you would expect? 
+# to the terminal as you would expect?
--- section1/reflection.md
+++ section1/reflection.md
@@ -1,19 +1,29 @@
 ## Section 1 Reflection
 
 1. Regarding the blog posts in Part A, how do you feel about asking questions? Do you tend to ask them too soon, or wait too long, or somewhere in between?
+ * Somewhere in between
 
 2. How would you print the string `"Hello World!"` to the terminal?
+ * print "Hello World!"
 
 3. What character is used to indicate comments in a ruby file?
+ * If it ends as an .rb file
 
 4. Explain the difference between an integer and a float?
+ * An integer is a number without a decimal while a float is a number with a decimal
 
 5. In the space below, create a variable `animal` that holds the string `"zebra"`
+ * animal = "zebra"
 
 6. How would you print the string `"zebra"` using the variable that you created above?
+ * p "Wow, look at that #{animal}"
 
 7. What is interpolation? Use interpolation to print a sentence using the variable `animal`.
+ * An interpolation is plugging in 2 variables in a line. Ex: p "Wow, look at that #[animal] it looks so #{Insert_another_variable_here}"
 
 8. What method is used to get input from a user?
+ * in ruby, it uses the "gets method"
 
 9. Name and describe two common string methods:
+ 1. The "get the length string method" is used to check the length of the string before inserting it into a data base. From my understanding you just have to put ".length" after the code you put in ex: "Hello World.length"
+ 2. Another one that is commonly used is the "Empty sting check method". It is used for data validation. You just need to put ".empty" after the code you want to use. Ex: "output.empty"
