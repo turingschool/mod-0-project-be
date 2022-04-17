74937e22a204d43cd9f4d7c78238b9edaaacc4f1
Jared Hardinger <jaredhardinger@gmail.com>
2022-04-16 23:15:53 -0400

    Add Section 3 work

--- section3/exercises/hashes.rb
+++ section3/exercises/hashes.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section3/exercises/hashes.rb`
 
 # Example: Write code that prints a hash holding grocery store inventory:
@@ -8,25 +8,26 @@ foods = {apples: 23, grapes: 507, eggs: 48}
 p foods
 
 # Write code that prints a hash holding zoo animal inventory:
-zoo = #YOUR CODE HERE
+zoo = {zebras: 46, lions:7, elephants: 984, snakes: 1}
 p zoo
 
-# Write code that prints all of the 'keys' of the zoo variable 
+# Write code that prints all of the 'keys' of the zoo variable
 # you created above:
 # YOUR CODE HERE
-
-# Write code that prints all of the 'values' of the zoo variable 
+p zoo.keys
+# Write code that prints all of the 'values' of the zoo variable
 # you created above:
 # YOUR CODE HERE
-
-# Write code that prints the value of the first animal of the zoo variable 
+p zoo.values
+# Write code that prints the value of the first animal of the zoo variable
 # you created above:
 # YOUR CODE HERE
-
-# Write code that adds an animal to the zoo hash. 
+p zoo[:zebras]
+# Write code that adds an animal to the zoo hash.
 # Then, print the updated hash:
 # YOUR CODE HERE
-
+zoo[:ferrets] = 15
+p zoo
 
 #-------------------
 # Part 2: Email
@@ -38,18 +39,19 @@ p zoo
 
 # Declare a variable that stores hash. Each key should be an attribute of an email and each
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
+email = {sender_email: "bob@zmail.com", sender_name: "Bob Johnson", subject: "This is not spam", date: "12 April 2022", time: "14:02"}
 
 # Write code that prints your email hash to the terminal.
+p email
 
-
-# Write code that prints all of the 'keys' of the email hash 
+# Write code that prints all of the 'keys' of the email hash
 # you created above:
 # YOUR CODE HERE
-
-# Write code that prints all of the 'values' of the email hash 
+p email.keys
+# Write code that prints all of the 'values' of the email hash
 # you created above:
 # YOUR CODE HERE
-
+p email.values
 
 #-------------------
 # Part 3: Many Emails - OPTIONAL EXTENSION
@@ -76,7 +78,7 @@ posts = [
     timestamp: "4:37 PM August 13, 2019",
     number_likes: 0,
     comments: []
-  }, 
+  },
   {
     image_src: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -90,12 +92,37 @@ p posts
 p posts[0]
 
 
-# The code snippet above shows an Array with 2 elements. Each element in the Array is a 
-# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK 
+# The code snippet above shows an Array with 2 elements. Each element in the Array is a
+# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK
 # a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 # good to have some exposure before going into Mod 1.
 
 
-# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same 
+# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same
 # key-value pairs you used in your email Hash above.
 # Then, print the email Array to the terminal.
+
+email = [
+  {
+    sender_email: "bob@zmail.com",
+    sender_name: "Bob Johnson",
+    subject: "This is not spam",
+    date: "12 April 2022",
+    time: "14:02"
+  },
+  {
+    sender_email: "prince@nigeria.xyz",
+    sender_name: "Prince Steve",
+    subject: "Claim your inheritance",
+    date: "10 April 2022",
+    time: "21:24"
+  },
+  {
+    sender_email: "mom@yahoo.com",
+    sender_name: "Mom",
+    subject: "I accidentally blocked every number in my phone please help",
+    date: "13 April 2022",
+    time: "06:49"
+  }
+]
+p email
--- section3/reflection.md
+++ section3/reflection.md
@@ -1,17 +1,25 @@
 ## Section 3 Reflection
 
 1. How different did your workflow feel this week, considering we asked you to follow the Pomodoro technique?
-
+*  I feel as if I had already been using a version of this technique: I fill up a glass of water/cup of tea/etc and work until I finish off that drink. When it's finished, I get up to take a quick break and get a refill. Focusing is important, but so is hydration!
 2. Regarding the work you did around setting intentions in Step 1 of the Pomodoro technique - how did that go? Were you surprised by anything (did you find yourself way more focused than you realized, more distracted that you thought you'd be, estimating times accurately or totally off, etc)?
-
+*  Setting intentions helped when I made the goal something I could accomplish in 30 mins to an hour. Any longer and there are too many tasks and it's easier to get overwhelmed and notice time dragging on. Less than 30 mins and I don't feel productive enough. Pomodoro definitely helps my mind focus more than having simple list of things to do with no clear timeframe to complete. I need to try it for getting my house projects accomplished in a more timely manner!
 3. What is a Hash, and how is it different from an Array?
-
+*  A hash matches some items (keys) with other items (values). This differs from an array in that the order doesn't matter, it's simply a 'look-up table'
 4. In the space below, create a Hash stored to a variable named `pet_store`.  This hash should hold an inventory of items and the number of that item that you might find at a pet store.
-
+```ruby
+pet_store = {cat_food: 27, cat_toys: 526, cats: 2}
+```
 5. Given the following `states = {"CO" => "Colorado", "IA" => "Iowa", "OK" => "Oklahoma"}`, how would you access the value `"Iowa"`?
-
+```ruby
+states[:IA]
+```
 6. With the same hash above, how would we get all the keys?  How about all the values?
-
+```ruby
+states.keys
+states.values
+```
 7. What is another example of when we might use a hash?  In your example, why is a hash better than an array?
-
+*  A dictionary. If I want to look up the definition of 'erudite' I don't want to have to know the index; I want to be able to input the work and receive the definition in return.
 8. What questions do you still have about hashes?
+*  There's so much that I don't know that I don't know--I'm sure questions will come, but there are none at this moment [that google hasn't already answered]
