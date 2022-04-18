32acf21f44288169b57e44c1e9aac30707796c3b
Nikky Rojas <rojas7313@gmail.com>
2022-04-18 16:21:08 -0600

    complete ex32, arrays, hashes

--- section3/exercises/arrays.rb
+++ section3/exercises/arrays.rb
@@ -5,7 +5,7 @@
 # Two examples are already completed. Your task is to complete
 # any remaining prompt.
 
-# To check your work, run this file by entering the following 
+# To check your work, run this file by entering the following
 # command in your terminal:
 # `ruby section3/exercises/arrays.rb``
 
@@ -18,23 +18,26 @@
 # Store the array in a variable.
 animals = ["Zebra", "Giraffe", "Elephant"]
 print animals
-
+puts
 # EXAMPLE: Write code below that will print "Zebra" from the animals array
 # YOUR CODE HERE
-print animals[0]
+puts animals[0]
 
 # YOU DO: Write code below that will print the number of elements in array of
 # animals from above.
-
+puts animals.length()
 
 # YOU DO: Write code that will reassign the last item in the animals
 # array to "Gorilla"
-
+animals [-1] = "Gorilla"
+puts animals
 
 # YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
-
+animals << "Lion"
+puts animals
 # YOU DO: Write code that will print the String "Elephant" in the animals array
+animals << 'Elephant'
+puts animals [-1]
 
 
 #-------------------
@@ -42,30 +45,40 @@ print animals[0]
 #-------------------
 
 # YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+foods = ['hambugers', 'pizza', 'nachos', 'chips']
 
 # YOU DO: Write code below that will print the number of elements in the array of
 # foods from above.
-
+print foods.length
 
 # YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 # print the changed array to verify "broccoli" has been added
 
+foods << 'broccoli'
+print foods
 
 # YOU DO: Write code below that removes the last item of food from the foods array and
 # print the changed array to verify that item has been removed
-
-
-# YOU DO: Write code to add 3 new foods to the array. 
+puts
+foods.pop
+print foods
+# YOU DO: Write code to add 3 new foods to the array.
   # There are several ways to do this - choose whichever you'd like!
 # Then, print the changed array to verify the new items have been added
-
+puts
+foods << 'pickles'
+foods << 'tomatoes'
+foods << 'letuce'
+print foods
+puts
+puts
 # YOU DO: Remove the food that is in index position 0.
-
+foods.delete_at (0)
+print foods
 #-------------------
 # PART 3: Where are Arrays used?
 #-------------------
-
+#Arrays are used when you need to contain large amounts of data (intergers, string, or booleans)
 
 # Sometimes we need to hold on to multiple pieces of data, but have it grouped together in a list.
 # This is why programming languages have arrays!
@@ -79,11 +92,12 @@ print animals[0]
 posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"]
 
 # YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-# may be storing data? Come up with 3 examples - they could be from different web applications or 
+# may be storing data? Come up with 3 examples - they could be from different web applications or
 # all from the same one.
 
-# 1:
-# 2:
-# 3:
-
-
+# 1: Facebook has a list of upcoming events and in each event it has list/ data (description, time, place, etc..)
+# 2: When ordering food online (wendys) you are provided a list of categories to choose from,
+# within each category you have more specific options and with in those options you can remove or add produce that
+# are shown as a list as well
+# 3: When booking services you may see a list a list of what that particular company provides. (facials, waxing, peels, etc.)
+#Each particular service may have it own description, price, and time duration.
--- section3/exercises/hashes.rb
+++ section3/exercises/hashes.rb
@@ -1,55 +1,72 @@
+pet_store = {collars:52, toys:76, leashes:32}
+puts
+print pet_store
+
+
+
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section3/exercises/hashes.rb`
 
 # Example: Write code that prints a hash holding grocery store inventory:
 foods = {apples: 23, grapes: 507, eggs: 48}
 p foods
-
 # Write code that prints a hash holding zoo animal inventory:
-zoo = #YOUR CODE HERE
+zoo = {elephants: 2, penguins: 20, birds: 12}#YOUR CODE HERE
 p zoo
 
-# Write code that prints all of the 'keys' of the zoo variable 
+
+# Write code that prints all of the 'keys' of the zoo variable
 # you created above:
 # YOUR CODE HERE
-
-# Write code that prints all of the 'values' of the zoo variable 
+foods.each { |key, values| puts key}
+# Write code that prints all of the 'values' of the zoo variable
 # you created above:
+puts
 # YOUR CODE HERE
+foods.each { |key, values| puts values}
+puts
+puts
 
-# Write code that prints the value of the first animal of the zoo variable 
+# Write code that prints the value of the first animal of the zoo variable
 # you created above:
-# YOUR CODE HERE
+p zoo[:elephants]
 
-# Write code that adds an animal to the zoo hash. 
+# Write code that adds an animal to the zoo hash.
 # Then, print the updated hash:
 # YOUR CODE HERE
-
+zoo.merge! :bear => 4, :rhino => 6 #found this way as well
+zoo[:tiger] = 3
+p zoo
 
 #-------------------
 # Part 2: Email
 #-------------------
-
+puts
 
 # Think about all the pieces of information associated with one single email in your inbox.
 # It has a sender, a subject, ...
 
 # Declare a variable that stores hash. Each key should be an attribute of an email and each
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
+#email = {"from" => "nikky", "to" => "family", "subject" => "special_note", "compose_email" => "i_love_you"}
+email = {from: "nikky", to: "family", subject: "special_note", compose_email: "i_love_you"}
 
 # Write code that prints your email hash to the terminal.
+puts email
 
-
-# Write code that prints all of the 'keys' of the email hash 
+# Write code that prints all of the 'keys' of the email hash
 # you created above:
 # YOUR CODE HERE
+email.each { |key,value | puts key}
 
-# Write code that prints all of the 'values' of the email hash 
+# Write code that prints all of the 'values' of the email hash
 # you created above:
-# YOUR CODE HERE
 
+email.each {|key, value| puts value}
+# YOUR CODE HERE
+puts
 
 #-------------------
 # Part 3: Many Emails - OPTIONAL EXTENSION
@@ -76,7 +93,7 @@ posts = [
     timestamp: "4:37 PM August 13, 2019",
     number_likes: 0,
     comments: []
-  }, 
+  },
   {
     image_src: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -89,13 +106,37 @@ posts = [
 p posts
 p posts[0]
 
+puts
+puts
 
-# The code snippet above shows an Array with 2 elements. Each element in the Array is a 
-# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK 
+# The code snippet above shows an Array with 2 elements. Each element in the Array is a
+# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK
 # a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 # good to have some exposure before going into Mod 1.
 
 
-# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same 
+# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same
 # key-value pairs you used in your email Hash above.
 # Then, print the email Array to the terminal.
+emails = ["nikky", "hubby", "mom"]
+emails = [
+  {
+    from: "nikky",
+    to: "family",
+    subject: "special_note",
+    compose_email: "i love you!!!"
+  },
+  {
+    from: "husband",
+    to: "nikky",
+    subject: "re: i love you!!!",
+    compose_email: "i love you too!"
+  },
+  {
+    from: "mom",
+    to: "nikky",
+    subject: "re: i love you too!",
+    compose_email: "Love you too hunny, miss you!"
+  }
+]
+puts emails
