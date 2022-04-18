aed6a7ebc23b5a43e499a4e9c7d9172beae68d8a
Ethan Nguyen <etnguyen16@gmail.com>
2022-04-17 21:25:39 -0600

    add ex34.rb

---
+++ section3/exercises/ex34.rb
@@ -0,0 +1,13 @@
+# The animal at 1 is a ruby
+# The third (3rd) animal is a peacock
+# The first (1st) animal is a bear
+# The animal at 3 is a ruby
+# The fifth (5th) animal is a whale
+# The animal at 2 is a peacock
+# The sixth (6th) animal is a platypus
+# The animal at 4 is a whale
+
+# ***study drills***
+# 1. there is no year zero in our calendar; therefore, it is really 2010
+students = ["Ethan", "Emma", "Bob", "Frank", "Caitlyn"]
+print students[2]
