9c9d8479379d5e4ba5c71abf29f3993db4455e62
Ben Silverstein <benjsilverstein@comcast.net>
2022-04-22 18:03:22 -0600

    Add hashes.rb

--- section3/exercises/hashes.rb
+++ section3/exercises/hashes.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section3/exercises/hashes.rb`
 
 # Example: Write code that prints a hash holding grocery store inventory:
@@ -8,24 +8,25 @@ foods = {apples: 23, grapes: 507, eggs: 48}
 p foods
 
 # Write code that prints a hash holding zoo animal inventory:
-zoo = #YOUR CODE HERE
+zoo = {monkeys: 2, alligators: 4, elephants: 6, zebras: 8}
 p zoo
 
-# Write code that prints all of the 'keys' of the zoo variable 
+# Write code that prints all of the 'keys' of the zoo variable
 # you created above:
-# YOUR CODE HERE
+puts zoo.keys
 
-# Write code that prints all of the 'values' of the zoo variable 
+# Write code that prints all of the 'values' of the zoo variable
 # you created above:
-# YOUR CODE HERE
+puts zoo.values
 
-# Write code that prints the value of the first animal of the zoo variable 
+# Write code that prints the value of the first animal of the zoo variable
 # you created above:
-# YOUR CODE HERE
+puts zoo.values[0]
 
-# Write code that adds an animal to the zoo hash. 
+# Write code that adds an animal to the zoo hash.
 # Then, print the updated hash:
-# YOUR CODE HERE
+zoo[:crocodiles] = 10
+p zoo
 
 
 #-------------------
@@ -40,15 +41,23 @@ p zoo
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
 
 # Write code that prints your email hash to the terminal.
+email = {
+  'Sender' => 'bsilverstein@comcast.net',
+  'Recipient' => 'msilverstein@comcast.net',
+  'Subject' => 'Funny Joke',
+  'BCC' => 'dkim@comcast.net',
+  'CC' => 'mlombardo@comcast.net'
+}
 
+puts email
 
-# Write code that prints all of the 'keys' of the email hash 
+# Write code that prints all of the 'keys' of the email hash
 # you created above:
-# YOUR CODE HERE
+puts email.keys
 
-# Write code that prints all of the 'values' of the email hash 
+# Write code that prints all of the 'values' of the email hash
 # you created above:
-# YOUR CODE HERE
+puts email.values
 
 
 #-------------------
@@ -76,7 +85,7 @@ posts = [
     timestamp: "4:37 PM August 13, 2019",
     number_likes: 0,
     comments: []
-  }, 
+  },
   {
     image_src: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -89,13 +98,41 @@ posts = [
 p posts
 p posts[0]
 
-
-# The code snippet above shows an Array with 2 elements. Each element in the Array is a 
-# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK 
+# The code snippet above shows an Array with 2 elements. Each element in the Array is a
+# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK
 # a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 # good to have some exposure before going into Mod 1.
 
 
-# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same 
+# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same
 # key-value pairs you used in your email Hash above.
 # Then, print the email Array to the terminal.
+
+email = ["Happy Birthday", "Happy New Year", "Car's Extended Warranty"]
+
+email = [
+  {
+    'Sender' => 'bsilverstein@comcast.net',
+    'Recipient' => 'msilverstein@comcast.net',
+    'Subject' => 'Happy Birthday, Michael',
+    'BCC' => 'dkim@comcast.net',
+    'CC' => 'mlombardo@comcast.net'
+  },
+  {
+    'Sender' => 'bsilverstein@comcast.net',
+    'Recipient' => 'msilverstein@comcast.net',
+    'Subject' => 'Happy New Year, Michael',
+    'BCC' => 'dkim@comcast.net',
+    'CC' => 'mlombardo@comcast.net'
+  },
+  {
+    'Sender' => 'bsilverstein@comcast.net',
+    'Recipient' => 'msilverstein@comcast.net',
+    'Subject' => "Michael, How's the Car Warranty?",
+    'BCC' => 'dkim@comcast.net',
+    'CC' => 'mlombardo@comcast.net'
+  }
+]
+
+puts email
+puts email[2]
