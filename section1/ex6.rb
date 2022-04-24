1a2a46e502d7a45297277eea952ed06934ee7369
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-04-24 13:33:50 -0600

    complete ex6

---
+++ section1/ex6.rb
@@ -0,0 +1,23 @@
+types_of_people = 10
+x = "There are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x #"There are {10} types of people"
+puts y #Those who know binary and those who don't
+
+puts "I said #{x}."
+puts "I also said: '#{y}'."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e # prints w then e with no space aside from what was in the string
+
+# This is the 5th exercise but we skipped to Ex6: Strings and Text
