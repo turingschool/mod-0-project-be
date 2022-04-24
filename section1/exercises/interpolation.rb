11993a9ddfc9f3a33cdb62280f06c6bbf0123c8d
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-04-24 14:30:32 -0600

    complete interpolation ex

--- section1/exercises/interpolation.rb
+++ section1/exercises/interpolation.rb
@@ -15,20 +15,24 @@ p "The #{team} are #{name}'s favorite Quidditch team"
 speedy = "quick red fox"
 slow_poke = "lazy brown dog"
 
-p # YOUR CODE HERE
+p "The #{speedy} jumped over the #{slow_poke}"
 
 # Write code that uses the variables below to form a string that reads
 # "In a predictable result, the tortoise beat the hare!":
 slow_poke = "tortoise"
 speedy = "hare"
 
-# YOUR CODE HERE
+p "In a predictable result, the #{slow_poke} beat the #{speedy}!"
 
 
 # YOU DO:
-# Declare three variables, name/content/data type of your choice. Think carefully 
-# about what you name the variables. Remember, the goal is to be concise but 
-# descriptive (it's a hard balance!) Then, print ONE sentence that incorporates 
+# Declare three variables, name/content/data type of your choice. Think carefully
+# about what you name the variables. Remember, the goal is to be concise but
+# descriptive (it's a hard balance!) Then, print ONE sentence that incorporates
 # all THREE variables.
 
-# YOUR CODE HERE
+emotional = "pets"
+spirital = "nature"
+health = "good food"
+
+p "The three most important things to consider surronding yourself in life with is with love and life such as with #{emotional}, fuffilling your soul by engaging with #{spirital} often, and always enjoying #{health}"
