89bb4e12e1d4a013f3f42bf62ca7398655a8d61c
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-04-24 14:23:52 -0600

    complete variables ex

--- section1/exercises/variables.rb
+++ section1/exercises/variables.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section1/exercises/variables.rb`
 
 # Example: Write code that saves your name to a variable and
@@ -11,33 +11,46 @@ p name
 # Write code that saves the string 'Dobby' to a variable and
 # prints what that variable holds to the terminal:
 house_elf = "Dobby"
-# YOUR CODE HERE
+p house_elf
 
 # Write code that saves the string 'Harry Potter must not return to Hogwarts!'
 # and prints what that variable holds to the terminal:
-# YOUR CODE HERE
+elf_line = 'Harry Potter must not return to Hogwarts!'
+p elf_line
 
 # Write code that adds 2 to the `students` variable and
 # prints the result:
 students = 22
-# YOUR CODE HERE
+students = students + 2
 p students
 
 # Write code that subracts 2 from the `students` variable and
 # prints the result:
-# YOUR CODE HERE
+students = students - 2
 p students
 
 
 # YOU DO:
-# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`. 
+# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`.
 # Store the appropriate data types in each.
 # print all three variables to the terminal.
 
-# IN WORDS:
-# How did you decide to use the data type you did for each of the three variables above? 
+first_name = "Sandy" #string
+is_hungry = true #boolean
+number_of_pets = 2 #integer
 
+p first_name
+p is_hungry
+p number_of_pets
+
+# IN WORDS:
+# How did you decide to use the data type you did for each of the three variables above?
 # Explain.
+
+# I chose a string to assign my name because it is a series of characters, and then I chose a booleans
+# variable for is_hungry because it poses as a YES/NO or TRUE/FALSE question, and then I chose an
+# integer for number_of_pets because that would be a accurate whole representation of the quantitiy
+# - not a decimal.
 
 
 # YOU DO:
@@ -45,11 +58,20 @@ p students
 # different values (but same data type).
 # Print all three variables to the terminal.
 
+first_name = "Marie"
+is_hungry = false
+number_of_pets = 16
+
+p first_name
+p is_hungry
+p number_of_pets
 
 # YOU DO:
 # Using the variables below, print the total number of snacks to the terminal:
 healthy_snacks = 6;
 junk_food_snacks = 8;
+
+p healthy_snacks + junk_food_snacks
 
 
 #-------------------
@@ -57,4 +79,4 @@ junk_food_snacks = 8;
 #-------------------
 
 # Did you run this file in your terminal to make sure everything printed out
-# to the terminal as you would expect? 
+# to the terminal as you would expect?
