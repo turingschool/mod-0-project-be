bf404473f9da8279433bd15b01dc41a70c6acc9c
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-04-24 12:55:31 -0600

    complete ex3

---
+++ section1/ex3.rb
@@ -0,0 +1,23 @@
+puts "I will now count my chickens:"
+# remember PEMDAS
+puts "Hens #{25 + 30 / 6}" # 25 + (30/6 = 5) = 30
+puts "Roosters #{100 - 25 * 3 % 4}" #100 - ([25 * 3] % 4) => 100 - (75 % 4 = 3)= 97
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 # not positive how this equals  7 yet
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7 #false
+
+puts "What is 3 + 2? #{3 + 2}" # = 5
+puts "What is 5 - 7? #{5 - 7}" # = -2
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}" #true
+puts "Is it greater or equal? #{5 >= -2}" #true
+puts "Is it less or equal? #{5 <= -2}" #false
