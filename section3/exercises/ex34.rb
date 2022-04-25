4a6e82d34877e42a42307ab3bed98e91afec8139
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-04-25 03:47:09 -0600

    complete accessing elements of arrays

---
+++ section3/exercises/ex34.rb
@@ -0,0 +1,13 @@
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+bear = animals[0]
+
+animals =['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+#1. The animal at 1 = ruby
+#2. The third animal = peacock
+#3. The first animal = bear
+#4. The animal at 3 = kangaroo
+#5. The fifth animal = whale
+#6. The animal at 2 = peacock
+#7. The sixth animal = platypus
+#8. The animal at 4 = whale
