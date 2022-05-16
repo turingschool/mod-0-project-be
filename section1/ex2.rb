66514ee6905e2af08a19f587c166f8164cd62e26
A.J. Krumholz <krumholz.alan@gmail.com>
2022-05-16 18:43:41 -0400

    Add comments to ex2

---
+++ section1/ex2.rb
@@ -0,0 +1,7 @@
+# Comments will not execute, even if they contain executable code.
+# They are useful for keeping track of what a specific section of
+# code may do, and for helping others who may be reading or working
+# on your code.
+
+print "This will show up!"
+# print "This won't."
