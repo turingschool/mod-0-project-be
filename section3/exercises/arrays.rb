257237a3b9a4c8708730a96ac0623c3dde774e1b
Ashley <ashuhleyt@gmail.com>
2022-05-24 16:27:31 -0600

    Add Section 3 Work

--- section3/exercises/arrays.rb
+++ section3/exercises/arrays.rb
@@ -5,7 +5,7 @@
 # Two examples are already completed. Your task is to complete
 # any remaining prompt.
 
-# To check your work, run this file by entering the following 
+# To check your work, run this file by entering the following
 # command in your terminal:
 # `ruby section3/exercises/arrays.rb``
 
@@ -25,42 +25,51 @@ print animals[0]
 
 # YOU DO: Write code below that will print the number of elements in array of
 # animals from above.
+animals.amount
 
 
 # YOU DO: Write code that will reassign the last item in the animals
 # array to "Gorilla"
+animals = ["Zebra", "Giraffe", "Gorilla"]
 
 
 # YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+animals = ["Zebra" "Giraffe," "Gorilla", "Whale"]
 
 # YOU DO: Write code that will print the String "Elephant" in the animals array
-
+animals = ["Zebra", "Giraffe", "Elephant"]
+animals[2]
 
 #-------------------
 # PART 2: Foods: Array Methods
 #-------------------
 
 # YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+vegetables = ["zucchini", "onion", "mushroom", "beet"]
 
 # YOU DO: Write code below that will print the number of elements in the array of
 # foods from above.
+vegetables.count
 
 
 # YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 # print the changed array to verify "broccoli" has been added
+vegetables.push('brocoli')
 
 
 # YOU DO: Write code below that removes the last item of food from the foods array and
 # print the changed array to verify that item has been removed
+vegetables.pop
 
 
-# YOU DO: Write code to add 3 new foods to the array. 
+# YOU DO: Write code to add 3 new foods to the array.
   # There are several ways to do this - choose whichever you'd like!
 # Then, print the changed array to verify the new items have been added
+vegetables = ["zucchini", "onion", "mushroom", "beet", "green beans", "shallot",
+"hearts of palm"]
 
 # YOU DO: Remove the food that is in index position 0.
+vegetables.shift
 
 #-------------------
 # PART 3: Where are Arrays used?
@@ -79,11 +88,9 @@ print animals[0]
 posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"]
 
 # YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-# may be storing data? Come up with 3 examples - they could be from different web applications or 
+# may be storing data? Come up with 3 examples - they could be from different web applications or
 # all from the same one.
 
-# 1:
-# 2:
-# 3:
-
-
+# 1:Reddit
+# 2:Facebook
+# 3:BeReal
--- section3/reflection.md
+++ section3/reflection.md
@@ -1,17 +1,25 @@
 ## Section 3 Reflection
 
 1. How different did your workflow feel this week, considering we asked you to follow the Pomodoro technique?
+really good! The one thing I struggled with is if I was on a roll, and really making progress on a challenge or activity, it was harder for me to step away for 3ish minutes and give myself the space. Productivity feels good, and finally understanding the things we are learning and implementing them feels even better!
 
 2. Regarding the work you did around setting intentions in Step 1 of the Pomodoro technique - how did that go? Were you surprised by anything (did you find yourself way more focused than you realized, more distracted that you thought you'd be, estimating times accurately or totally off, etc)?
+I was very focused, and the time FLEW by. I am enjoying this section specifically, and am absorbing and feel very confident in it. Setting intentions feels really good, as doing this and a FT job + social life is helping me make sure I accomplish everything.
 
 3. What is a Hash, and how is it different from an Array?
+Hash has multiple data points and can have subsequently more data points, whereas, an array is allowing multiple data points to be stored under the same name.
 
 4. In the space below, create a Hash stored to a variable named `pet_store`.  This hash should hold an inventory of items and the number of that item that you might find at a pet store.
+pet_store = {'dogs' => 1000, 'cats' => 2, 'birds' => 666}
 
 5. Given the following `states = {"CO" => "Colorado", "IA" => "Iowa", "OK" => "Oklahoma"}`, how would you access the value `"Iowa"`?
+states["IA"]
 
 6. With the same hash above, how would we get all the keys?  How about all the values?
+states.keys
+states.values
 
-7. What is another example of when we might use a hash?  In your example, why is a hash better than an array?
+7. What is another example of when we might use a hash?  In your example, why is a hash better than an array? At an animal shelter, a hash would be better to use regarding all of the information a potential adoptee would want to know about the dog they are interested in adopting. An array would just give us basic information like, name. Using a hash, we could have the name, age, breed, eye color etc listed.
 
 8. What questions do you still have about hashes?
+A few, sometimes googling is more confusing than it is helpful as the information out there can be overwhelming, and it *seems* there are a lot of different solutions, even when there isn't.
