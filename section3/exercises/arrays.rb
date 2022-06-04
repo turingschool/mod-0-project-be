6b4853fd031e687a86491ea022735f7e60fe6a95
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-06-04 11:41:05 -0400

    Add section3 practice

--- section3/exercises/arrays.rb
+++ section3/exercises/arrays.rb
@@ -5,7 +5,7 @@
 # Two examples are already completed. Your task is to complete
 # any remaining prompt.
 
-# To check your work, run this file by entering the following 
+# To check your work, run this file by entering the following
 # command in your terminal:
 # `ruby section3/exercises/arrays.rb``
 
@@ -25,17 +25,17 @@ print animals[0]
 
 # YOU DO: Write code below that will print the number of elements in array of
 # animals from above.
-
+print "number of animals : #{animals.count}"
 
 # YOU DO: Write code that will reassign the last item in the animals
 # array to "Gorilla"
-
-
+animals << "Gorilla"
+print animals
 # YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
-
+animals.insert(3, "Hawk")
+print animals
 # YOU DO: Write code that will print the String "Elephant" in the animals array
-
+print animals[2]
 
 #-------------------
 # PART 2: Foods: Array Methods
@@ -56,7 +56,7 @@ print animals[0]
 # print the changed array to verify that item has been removed
 
 
-# YOU DO: Write code to add 3 new foods to the array. 
+# YOU DO: Write code to add 3 new foods to the array.
   # There are several ways to do this - choose whichever you'd like!
 # Then, print the changed array to verify the new items have been added
 
@@ -79,11 +79,9 @@ print animals[0]
 posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"]
 
 # YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-# may be storing data? Come up with 3 examples - they could be from different web applications or 
+# may be storing data? Come up with 3 examples - they could be from different web applications or
 # all from the same one.
 
 # 1:
 # 2:
 # 3:
-
-
---
+++ section3/notes.md
@@ -0,0 +1,23 @@
+•	Array
+a container of things that are organized in order from first to last
+ex. hairs = ['brown', 'blond', 'red']
+You start the array with the [ (left bracket) which "opens" the array. Then you put each item you want in the array separated by commas, similar to function arguments. Lastly, end the array with a ] (right bracket) to indicate that it's over. Ruby then takes this array and all its contents and assigns them to the variable.
+you can only use numbers to get items out of a array.
+•	element
+•	Hash
+ like an array but you use anything, not just numbers as your index.  A hash is used to map or associate things you want to store to keys you need to get them. A hash in Ruby is just like a dictionary in the real world - mapping a word to its definition.  "mapping" or "associating" is the key concept in a hash
+
+ What is the difference between an array and a hash?
+ A array is for an ordered array of items. A hash (or hash) is for matching some items (called "keys") to other items (called "values").
+
+ What would I use a hash for?
+When you have to take one value and "look up" another value. In fact, you could call hashes "look up tables."
+What would I use an array for?
+Use an array for any sequence of things that need to be in order, and you only need to look them up by a numeric index.
+
+When creating a hash the key and value are linked by the => symbol which is called a rocket. So hashes start with a curly bracket {, have zero or more entries made up of a key, a rocket, and a value separated by commas, then end with a closing curly bracket }.
+
+•	key
+on the left of the hash pair (index/address)
+•	value
+on the right (definition)
