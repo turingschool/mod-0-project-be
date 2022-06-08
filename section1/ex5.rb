6572efde594959dde4ef4b79060b7d16a049005d
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-06-07 20:10:14 -0600

    Exercise 5

---
+++ section1/ex5.rb
@@ -0,0 +1,17 @@
+my_name = "Kaelin E. Sleevi"
+my_age = 23 #not a lie in 2009
+my_height = 59 #inches
+my_weight = 135 #lbs
+my_eyes = 'Blue/Green/Yellow'
+my_teeth = 'White'
+my_hair = 'Red'
+
+puts "Let's talk about #{my_name}."
+puts "She's #{my_height} inches tall."
+puts "She's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "She's got #{my_eyes} eyes and #{my_hair} hair."
+puts "Her teeth are usually #{my_teeth} depending on the coffee."
+
+#this line is a little tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
