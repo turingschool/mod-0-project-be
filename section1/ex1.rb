5b9915ec7c6dd48a8e0876ab0df6cbc8a3882948
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-06-11 20:56:56 -0600

    update answers for section 1: a good first program

--- section1/ex1.rb
+++ section1/ex1.rb
@@ -1,7 +1,17 @@
+Exercise
 puts "Hello World!"
-puts "Hello Again"
+#puts "Hello Again"
 puts "I like typing this."
-puts "This is fun."
+#puts "This is fun."
 puts "Yay! Printing."
-puts "I'd much rather you 'not'."
+#puts "I'd much rather you 'not'."
 puts 'I "said" do not touch this.'
+#puts "This is another line."
+
+Study Drills
+1. Make your script print another line.
+  - I added "puts 'This is another line.'" at the very end.
+2. Make your script print only one of the lines.
+  - I used an octothorpe to in front of each line except one.
+3. Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
+  - An octothorpe made ruby ignore the code following it.
