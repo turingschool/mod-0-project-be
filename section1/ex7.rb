c1d67a4a1bde730758c5683a23517117c597b388
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-06-11 21:00:48 -0600

    update answers for section 7: asking for input

--- section1/ex7.rb
+++ section1/ex7.rb
@@ -1,3 +1,4 @@
+Exercise
 print "How old are you? "
 age = gets.chomp
 print "How tall are you? "
@@ -6,3 +7,21 @@ print "How much do you weigh? "
 weight = gets.chomp
 
 puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+Study Drills
+1. Go online and find out what Rubys gets.chomp does.
+  - I found that the "gets" creates a new line and asks the user to input a integer, variable, float, etc. and gets.chomp does the same thing except that it does not create a new line. gets.chomp allows a user to input as well as "gets", but there will not be a new line that is added after the string.
+
+2. Can you find other ways to use it? Try some of the samples you find.
+You can use it ask questions to the user, play a game or run through the game play, you can use it to have users put their own information into the code.
+
+3. Write another "form" like this to ask some other questions.
+
+print "What is your favorite food?"
+food = gets.chomp
+print "What dessert do you like?"
+dessert = gets.chomp
+print "What would you like to drink?"
+drink = gets.chomp
+
+puts "So, you like #{food}, you would like #{dessert} and want #{drink} for a drink."
