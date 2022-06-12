d0749aef34c1d5772e27a5daac78a081c6df8ec8
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-06-11 20:57:32 -0600

    update answers for section 2: comments in code

--- section1/ex2.rb
+++ section1/ex2.rb
@@ -1,3 +1,4 @@
+Exercise
 # A comment, this is so you can read your program later.
 # Anything after the # is ignored by ruby.
 
@@ -7,3 +8,13 @@ puts "I could have code like this." # and the comment after is ignored
 # puts "This won't run."
 
 puts "This will run."
+
+Study Drills
+1. Find out if you were right about what the # character does and make sure you know what it's called (octothorpe or pound character)
+ - I was right about the octothorpe. The symbol is used to make note in the code without disturbing it. The symbol tells ruby to ignore it and everything after in that line.
+2. Take your ex2.rb file and review each line going backward. Start at the last line, and check each word in reverse against what you should have typed.
+
+3. Did you find more mistakes? Fix them.
+  - No
+4. Read what you typed above out loud, including saying each character by its name. Did you find more mistakes? Fix them.
+  - No
