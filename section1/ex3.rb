e92ccb3593011b75c518f316275f690e630550e0
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-06-11 20:58:11 -0600

    update answers for section 3: numbers and math

--- section1/ex3.rb
+++ section1/ex3.rb
@@ -1,23 +1,31 @@
+Exercise
+#prints the statement so I know what the code/equation is doing
 puts "I will now count my chickens:"
-
+#the amount of hens and roosters(integer) are calculated by the equation below
 puts "Hens #{25 + 30 / 6}"
-puts "Roosters #{100 - 25 * 3 % 4}"
-
+puts "Roosters #{100 - 25 * 3.0 % 4.0}"
+#prints a statment to know what the code/equation is now calculating
 puts "Now I will count the eggs:"
-
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
-
+#this equation calculates the amount of eggs into one integer/amount
+puts 3.0 + 2.0 + 1.0 - 5.2 + 4.0 % 2.1 - 1.3 / 4.0 + 6.5
+#asking a true or false question based on the equations
 puts "Is it true that 3 + 2 < 5 - 7?"
-
-puts 3 + 2 < 5 - 7
-
-puts "What is 3 + 2? #{3 + 2}"
-puts "What is 5 - 7? #{5 - 7}"
-
+#calculates this question in the equation to tell us if it is true or false
+puts 3.0 + 2.0 < 5.2 - 7.3
+#both line 16 and 17 are asking ruby to calculate the equations below.
+puts "What is 3 + 2? #{3.0 + 2.0}"
+puts "What is 5 - 7? #{5.2 - 7.3}"
+#this line loops back to the question on line 12. After the equations are calculated it is telling you that your asnwer is why the equation is false
 puts "Oh, that's why it's false."
-
+#prints a statement letting us know we are going to do more calculations
 puts "How about some more."
+#lines 23, 24, 25 are asking if eeach statement is true or false based on the calculations
+puts "Is it greater? #{5.2 > -2.0}"
+puts "Is it greater or equal? #{5.2 >= -2.0}"
+puts "Is it less or equal? #{5.2 <= -2.0}"
 
-puts "Is it greater? #{5 > -2}"
-puts "Is it greater or equal? #{5 >= -2}"
-puts "Is it less or equal? #{5 <= -2}"
+Study Drills
+1. Above each line, use the # to write a comment to yourself explaining what the line does.
+2. Remember in Exercise 0 when you started irb? Start irb this way again and, using the math operators, use Ruby as a calculator.
+3. Find something you need to calculate and write a new .rb file that does it.
+4. Rewrite ex3.rb to use floating point numbers so it's more accurate. 20.0 is floating point.
