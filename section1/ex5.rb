c3a9666d11dfd653955040e3f33e99fd387f6b73
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-06-11 20:59:33 -0600

    update answers for section 5: strings

--- section1/ex5.rb
+++ section1/ex5.rb
@@ -1,17 +1,33 @@
-my_name = "Kaelin E. Sleevi"
-my_age = 23 #not a lie in 2009
-my_height = 59 #inches
-my_weight = 135 #lbs
-my_eyes = 'Blue/Green/Yellow'
-my_teeth = 'White'
-my_hair = 'Red'
+Exercise
+myname = "Kaelin E. Sleevi"
+myage = 23 #not a lie in 2009
+myheight = 175.26 #centimeters
+myweight = 61.29 #kilograms
+myeyes = 'Blue/Green/Yellow'
+myteeth = 'White'
+myhair = 'Red'
 
-puts "Let's talk about #{my_name}."
-puts "She's #{my_height} inches tall."
-puts "She's #{my_weight} pounds heavy."
+puts "Let's talk about #{myname}."
+puts "She's #{myheight} inches tall."
+puts "She's #{myweight} pounds heavy."
 puts "Actually that's not too heavy."
-puts "She's got #{my_eyes} eyes and #{my_hair} hair."
-puts "Her teeth are usually #{my_teeth} depending on the coffee."
+puts "She's got #{myeyes} eyes and #{myhair} hair."
+puts "Her teeth are usually #{myteeth} depending on the coffee."
 
 #this line is a little tricky, try to get it exactly right
 puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+
+Study Drills
+1. Change all the variables so there is no my_ in front of each one. Make sure you change the name everywhere, not just where you used = to set them.
+
+2. Try to write some variables that convert the inches and pounds to centimeters and kilograms. Do not just type in the measurements. Work out the math in Ruby.
+irb(main):004:0> 12 * 5
+=> 60
+irb(main):005:0> 2.54 * 69
+=> 175.26
+irb(main):006:0>
+
+
+irb(main):006:0> 135 * 0.454
+=> 61.29
+irb(main):007:0>
