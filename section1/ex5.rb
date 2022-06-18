f247771acfdd6aede0b2920ef29aba37ffe84b02
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-06-18 16:41:13 -0600

    update exercise 5 format

--- section1/ex5.rb
+++ section1/ex5.rb
@@ -1,4 +1,4 @@
-Exercise
+#Exercise
 myname = "Kaelin E. Sleevi"
 myage = 23 #not a lie in 2009
 myheight = 175.26 #centimeters
@@ -17,17 +17,17 @@ puts "Her teeth are usually #{myteeth} depending on the coffee."
 #this line is a little tricky, try to get it exactly right
 puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
 
-Study Drills
-1. Change all the variables so there is no my_ in front of each one. Make sure you change the name everywhere, not just where you used = to set them.
+#Study Drills
+#1. Change all the variables so there is no my_ in front of each one. Make sure you change the name everywhere, not just where you used = to set them.
 
-2. Try to write some variables that convert the inches and pounds to centimeters and kilograms. Do not just type in the measurements. Work out the math in Ruby.
-irb(main):004:0> 12 * 5
-=> 60
-irb(main):005:0> 2.54 * 69
-=> 175.26
-irb(main):006:0>
+#2. Try to write some variables that convert the inches and pounds to centimeters and kilograms. Do not just type in the measurements. Work out the math in Ruby.
+#irb(main):004:0> 12 * 5
+#=> 60
+#irb(main):005:0> 2.54 * 69
+#=> 175.26
+#irb(main):006:0>
 
 
-irb(main):006:0> 135 * 0.454
-=> 61.29
-irb(main):007:0>
+#irb(main):006:0> 135 * 0.454
+#=> 61.29
+#irb(main):007:0>
