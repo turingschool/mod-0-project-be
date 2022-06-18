fbca80c549f8d02e39d2a27aa9af5c96a39c6e94
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-06-18 16:39:09 -0600

    update exercise 1 format

--- section1/ex1.rb
+++ section1/ex1.rb
@@ -1,4 +1,4 @@
-Exercise
+#Exercise
 puts "Hello World!"
 #puts "Hello Again"
 puts "I like typing this."
@@ -8,10 +8,10 @@ puts "Yay! Printing."
 puts 'I "said" do not touch this.'
 #puts "This is another line."
 
-Study Drills
-1. Make your script print another line.
-  - I added "puts 'This is another line.'" at the very end.
-2. Make your script print only one of the lines.
-  - I used an octothorpe to in front of each line except one.
-3. Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
-  - An octothorpe made ruby ignore the code following it.
+#Study Drills
+#1. Make your script print another line.
+  #- I added "puts 'This is another line.'" at the very end.
+#2. Make your script print only one of the lines.
+  #- I used an octothorpe to in front of each line except one.
+#3. Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
+  #- An octothorpe made ruby ignore the code following it.
