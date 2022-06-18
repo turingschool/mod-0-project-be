1b211785610db1596a5cdc518a3aa3b401ac7c58
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-06-18 16:40:41 -0600

    update exercise 4 format

--- section1/ex4.rb
+++ section1/ex4.rb
@@ -1,4 +1,4 @@
-Exercise
+#Exercise
 # the amount of cars in this scenario is 100
 cars = 100
 # the amount of space within the car for passengers is 4.0 in a car
@@ -24,32 +24,31 @@ puts "We can transport #{carpool_capacity} people today."
 puts "We have #{passengers} to carpool today."
 puts "We need to put about #{average_passengers_per_car} in each car."
 
-Study Drills
+#Study Drills
 
-```There are 100 cars available.```
-```There are only 30 drivers available.```
-```There will be 70 empty cars today.```
-```ex4.rb:14: undefined local variable or method "carpool_capacity" for
-    main:Object (NameError)```
+#```There are 100 cars available.```
+#```There will be 70 empty cars today.```
+#```ex4.rb:14: undefined local variable or method "carpool_capacity" for
+    #main:Object (NameError)```
 
-1. Explain this error in your own words. Make sure you use line numbers and explain why.
-  - In my own words I would say that you neglected to give a defined variable or a variable that was a float or integer.
-  - Say I printed this out but I neglected to define "carpool_capacity". It would look like this:
-  cars = 100
-space_in_a_car = 4.0
-drivers = 30
-passengers = 90
-cars_not_driven = cars - drivers
-cars_driven = drivers
-carpool_capacity =
-average_passengers_per_car = passengers / cars_driven
-  - The puts "We can transport #{carpool_capacity} people today" would not go through.
+#1. Explain this error in your own words. Make sure you use line numbers and explain why.
+  #- In my own words I would say that you neglected to give a defined variable or a variable that was a float or integer.
+  #- Say I printed this out but I neglected to define "carpool_capacity". It would look like this:
+  #cars = 100
+#space_in_a_car = 4.0
+#drivers = 30
+#passengers = 90
+#cars_not_driven = cars - drivers
+#cars_driven = drivers
+#carpool_capacity =
+#average_passengers_per_car = passengers / cars_driven
+  #- The puts "We can transport #{carpool_capacity} people today" would not go through.
 
-2. I used 4.0 for space_in_a_car, but is that necessary? What happens if its just 4?
-  - 4.0 and 4 are technically the same number, but 4 is an integer and 4.0 is a float. If you just use 4 the output will just be an integer.
+#2. I used 4.0 for space_in_a_car, but is that necessary? What happens if its just 4?
+  #- 4.0 and 4 are technically the same number, but 4 is an integer and 4.0 is a float. If you just use 4 the output will just be an integer.
 
-3. Remember that 4.0 is a floating point number. It's just a number with a decimal point, and you need 4.0 instead of just 4 so that it is floating point.
-3. Write comments above each of the variable assignments.
-4. Make sure you know what = is called (equals) and that its purpose is to give data (numbers, strings, etc.) names (cars_driven, passengers).
-5. Remember that _ is an underscore character.
-6. Try running ruby from the Terminal as a calculator like you did before, and use variable names to do your calculations. Popular variable names are also i, x, and j.
+#3. Remember that 4.0 is a floating point number. It's just a number with a decimal point, and you need 4.0 instead of just 4 so that it is floating point.
+#3. Write comments above each of the variable assignments.
+#4. Make sure you know what = is called (equals) and that its purpose is to give data (numbers, strings, etc.) names (cars_driven, passengers).
+#5. Remember that _ is an underscore character.
+#6. Try running ruby from the Terminal as a calculator like you did before, and use variable names to do your calculations. Popular variable names are also i, x, and j.
